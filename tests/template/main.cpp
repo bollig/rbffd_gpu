e5c2313981d4acf45d904c188946724e1473717a
bollig <bollig@92027bb4-6f2d-41e4-bb36-ce9f418afc6c>
2010-06-21 21:32:43 +0000

    template STILL had junk content. I dont know why this keeps returning.
    git-svn-id: https://svn.sc.fsu.edu/svn/habushu/RBF.framework/trunk@393 92027bb4-6f2d-41e4-bb36-ce9f418afc6c

--- tests/template/main.cpp
+++ tests/template/main.cpp
@@ -1,9 +1,6 @@
 #include <math.h>
 #include <stdlib.h>
 #include <vector>
-#include <ellipsoid_patch.h>
-#include <parametric_patch.h>
-#include <octree.h>
 
 using namespace std;
 
@@ -12,18 +9,6 @@ using namespace std;
 //----------------------------------------------------------------------
 int main (int argc, char** argv)
 {
-	// domain dimensions
-	double pi = acos(-1.);
-	double a = 2.;
-	double b = 3.;
-	double c = 4.;
-
-	int n1 = 40;
-	int n2 = 40;
-	ParametricPatch* ep = new EllipsoidPatch(0., pi, 0., 2.*pi, n1, n2, a, b, c);
-
-	Octree oct(ep);
-
 	if (argc > 1) {
 		return EXIT_FAILURE; 	// FAIL TEST
 	} 

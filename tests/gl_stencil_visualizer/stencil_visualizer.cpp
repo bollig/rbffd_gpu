757da2dccaefab511cb7787590f708ab33f04f9f
Evan Bollig <bollig@scs.fsu.edu>
2010-11-03 03:22:33 -0400

    Got a working QT4 application that does not depend on GLUT, which means we can have many instances of the viewer running at once. This also helps keep things C++ rather than C oriented.

--- tests/gl_stencil_visualizer/stencil_visualizer.cpp
+++ tests/gl_stencil_visualizer/stencil_visualizer.cpp
@@ -1,39 +1,583 @@
+#include <QtOpenGL/qgl.h>
+#include <QtGui/QKeyEvent>
+
 #include "stencil_visualizer.h"
+#include "visual_stencil.h"
 
-StencilVisualizer::StencilVisualizer(std::vector<Vec3> nodes, std::vector<double> weights) {
+#include <GLUT/glut.h>
+
+
+
+// Initialize function adapted from existing code
+
+void StencilVisualizer::initializeGL(void)
+{
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glShadeModel (GL_FLAT);
+}
+
+// Wire cube program to replace glutWireCube();
+
+void StencilVisualizer::wireCube(float length)
+{
+    glBegin(GL_LINE_LOOP);
+    glVertex3f(-length/2, -length/2, length/2);
+    glVertex3f(length/2, -length/2, length/2);
+    glVertex3f(length/2, length/2, length/2);
+    glVertex3f(-length/2, length/2, length/2);
+    glEnd();
+
+    glBegin(GL_LINE_LOOP);
+    glVertex3f(-length/2, -length/2, -length/2);
+    glVertex3f(length/2, -length/2, -length/2);
+    glVertex3f(length/2, length/2, -length/2);
+    glVertex3f(-length/2, length/2, -length/2);
+    glEnd();
+
+    glBegin(GL_LINES);
+    glVertex3f(-length/2, -length/2, length/2);
+    glVertex3f(-length/2, -length/2, -length/2);
+
+    glVertex3f(length/2, -length/2, length/2);
+    glVertex3f(length/2, -length/2, -length/2);
+
+    glVertex3f(length/2, length/2, length/2);
+    glVertex3f(length/2, length/2, -length/2);
+
+    glVertex3f(-length/2, length/2, length/2);
+    glVertex3f(-length/2, length/2, -length/2);
+    glEnd();
+}
+
+// paint function adapted from original display function
+
+void StencilVisualizer::paintGL(void)
+{
+    float len =3;
+    glClear (GL_COLOR_BUFFER_BIT);
+    glPushMatrix();
+    glTranslatef (-1.0, 0.0, 0.0);
+    glRotatef ((GLfloat) shoulder, 0.0, 0.0, 1.0);
+    glTranslatef (1.0, 0.0, 0.0);
+    glPushMatrix();
+    glScalef (2.0, 0.4, 1.0);
+    wireCube(1.0);
+
+    glPopMatrix();
+
+    glTranslatef (1.0, 0.0, 0.0);
+    glRotatef ((GLfloat) elbow, 0.0, 0.0, 1.0);
+    glTranslatef (1.0, 0.0, 0.0);
+    glPushMatrix();
+    glScalef (2.0, 0.4, 1.0);
+    wireCube(1.0);
+    glPopMatrix();
+
+    glPopMatrix();
+    //swapBuffers();
+}
+
+// Resize function adapted from reshape function
+
+void StencilVisualizer::resizeGL(int w, int h)
+{
+    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+    gluPerspective(65.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef (0.0, 0.0, -5.0);
+}
+
+// Key press event function to replace keyboard function.  This
+// function is modified to only take one argument (the keyboard input)
+// instead of receiving three, as in the keyboard function.
+
+void StencilVisualizer::keyPressEvent(QKeyEvent *e)
+{
+    switch (e->key()) {
+    case 'q':
+        this->close();
+        break;
+    case 'Q':
+        this->close();
+        break;
+    case 's':
+        shoulder = (shoulder + 5) % 360;
+        updateGL();
+        break;
+    case 'S':
+        shoulder = (shoulder - 5) % 360;
+        updateGL();
+        break;
+    case 'e':
+        elbow = (elbow + 5) % 360;
+        updateGL();
+        break;
+    case 'E':
+        elbow = (elbow - 5) % 360;
+        updateGL();
+        break;
+    case 27:    // Not sure what this key is. Is it ESC? Doesnt work on OSX, thats for sure.
+        exit(0);
+        break;
+    default:
+        break;
+    }
+}
+
+#if 0
+
+StencilVisualizer::StencilVisualizer(int argc, char** argv)
+{
+    winW = 800;
+    winH = 600;
+    screen_dirty = true;
+
+    initializeWindow(argc, argv);
+    glCheckErrors();
+
+
+    glCheckErrors();
+
+    setupLights();
+    glCheckErrors();
+
+    create_menu() ;
+    glCheckErrors();
+
+}
+
+StencilVisualizer::~StencilVisualizer()
+{
 
 }
 
 
-StencilVisualizer::~StencilVisualizer() {
+void StencilVisualizer::run()
+{
+    glutMainLoop();
+}
 
+void GLApp::Callbacks::display()
+{
+#if 0
+    unsigned i;
+
+    // Uploads a Vec to OGL
+#define UPLOADVEC(v) glVertex3f( v.x, v.y, v.z )
+
+    static GLfloat color1[] = {0.8, 0.8, 0.8, 1.0};
+    static GLfloat color2[] = {1.0, 1.0, 1.0, 1.0};
+
+    static GLfloat red[] = {1.0, 0.0, 0.0, 1.0};
+    static GLfloat green[] = {0.0, 1.0, 0.0, 1.0};
+    static GLfloat blue[] = {0.0, 0.0, 1.0, 1.0};
+
+    // Make a rotation matrix out of mouse point
+
+    const Mtx& rot = rotateY( camBeta ) * rotateX( camAlpha );
+
+    // Rotate camera
+    Vec3 cam_loc(0,0,-150), cam_up(0,1,0);
+    cam_loc = cam_loc * rot;
+    cam_up = cam_up * rot;
+
+    // Clear the screen
+    glClear( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT );
+
+    // Prepare zoom by changiqng FOV
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    float fov = 45 + camZoom;
+    if ( fov < 5 ) fov = 5;
+    if ( fov > 160 ) fov = 160;
+    gluPerspective( fov, (float)winW/(float)winH, 1.0, 500.0 );
+
+    gluLookAt( cam_loc.x, cam_loc.y, cam_loc.z,  // eye
+               0, 0, 0, // target
+               cam_up.x, cam_up.y, cam_up.z ); // up
+
+    // Curve surface
+    glEnable(GL_LIGHTING) ;
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glBegin( GL_QUADS );
+
+    static GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    static GLfloat mat_shininess[] = { 100.0 };
+
+    for ( int x=-GRID_W/2; x<GRID_W/2-1; ++x )
+    {
+        for ( int z=-GRID_H/2; z<GRID_H/2-1; ++z )
+        {
+            if ( (x&8)^(z&8) )
+                glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color1);
+            else
+                glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color2);
+
+            glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+            glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
+
+            // Evan: HERE we draw the cartesian grid overlay.
+            float a[] = { x+0, grid[x+0+GRID_W/2][z+0+GRID_H/2], z+0 };
+            float b[] = { x+0, grid[x+0+GRID_W/2][z+1+GRID_H/2], z+1 };
+            float c[] = { x+1, grid[x+1+GRID_W/2][z+1+GRID_H/2], z+1 };
+            float d[] = { x+1, grid[x+1+GRID_W/2][z+0+GRID_H/2], z+0 };
+
+#define V_MINUS(A,B) {A[0]-B[0], A[1]-B[1], A[2]-B[2]}
+#define V_CROSS(A,B) \
+            {A[1]*B[2]-A[2]*B[1], \
+             A[2]*B[0]-A[0]*B[2], \
+             A[0]*B[1]-A[1]*B[0]}
+            float ab[] = V_MINUS(a,b);
+            float cb[] = V_MINUS(c,b);
+            float n[] = V_CROSS( cb, ab );
+
+            glNormal3f( n[0],n[1],n[2] );
+            glVertex3f( a[0],a[1],a[2] );
+            glVertex3f( b[0],b[1],b[2] );
+            glVertex3f( c[0],c[1],c[2] );
+            glVertex3f( d[0],d[1],d[2] );
+        }
+    }
+    glEnd();
+
+#if 0 // visual helpers
+
+    glDisable(GL_LIGHTING) ;
+
+    // Flat grid
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glBegin( GL_QUADS );
+    glColor3ub( 128, 128, 128 );
+
+    for ( int x=-GRID_W/2; x<GRID_W/2-4; x+=5 )
+    {
+        for ( int z=-GRID_H/2; z<GRID_H/2-4; z+=5 )
+        {
+            glVertex3f( x-0.5, -0.5f, z-0.5 );
+            glVertex3f( x-0.5, -0.5f, z+4.5 );
+            glVertex3f( x+4.5, -0.5f, z+4.5 );
+            glVertex3f( x+4.5, -0.5f, z-0.5 );
+        }
+    }
+    glEnd();
+
+    // Axes: (x,y,z)=(r,g,b)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glPushMatrix();
+    glRotatef(90.0, 0.0, 1.0, 0.0);
+    glColor3ub( 255, 0, 0 );
+    glutSolidCone(0.5, 80.0, 5, 1);
+    glPopMatrix();
+    glPushMatrix();
+    glRotatef(-90.0, 1.0, 0.0, 0.0);
+    glColor3ub( 0, 255,  0 );
+    glutSolidCone(0.5, 80.0, 5, 1);
+    glPopMatrix();
+    glPushMatrix();
+    glRotatef(0.0, 1.0, 0.0, 0.0);
+    glColor3ub( 0, 0, 255 );
+    glutSolidCone(0.5, 80.0, 5, 1);
+    glPopMatrix();
+
+#endif
+
+    // Control points
+    int old_sel = selected_cp;
+    if ( mouseState[0] == mouseState[1] == mouseState[2] == 0 )
+        selected_cp = -1;
+
+    for ( int i=0; i < control_points.size(); ++i )
+    {
+        const Vec& cp = control_points[i];
+        if ( ( cp - cursor_loc ).len() < 2.0 )
+        {
+            selected_cp = i;
+            glutSetCursor( GLUT_CURSOR_UP_DOWN );
+        }
+
+        glPushMatrix();
+        glTranslatef(cp.x, cp.y, cp.z);
+        if ( selected_cp == i )
+            glColor3ub( 0, 255, 255 );
+        else
+            glColor3ub( 255, 255, 0 );
+        glutSolidSphere(1.0,12,12);
+        glPopMatrix();
+
+        glBegin( GL_LINES );
+        glVertex3f( cp.x, 0, cp.z );
+        glVertex3f( cp.x, cp.y, cp.z );
+        glEnd();
+    }
+
+    if ( selected_cp < 0 && old_sel != selected_cp )
+        glutSetCursor( GLUT_CURSOR_CROSSHAIR );
+
+    // Find out the world coordinates of mouse pointer
+    // to locate the cursor
+    if ( mouseState[0] == mouseState[1] == mouseState[2] == 0 )
+    {
+        GLdouble model[16], proj[16];
+        GLint view[4];
+        GLfloat z;
+        GLdouble ox, oy, oz;
+
+        glGetDoublev(GL_MODELVIEW_MATRIX, model);
+        glGetDoublev(GL_PROJECTION_MATRIX, proj);
+        glGetIntegerv(GL_VIEWPORT, view);
+
+        glReadPixels(mouseX, view[3]-mouseY-1, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &z);
+        gluUnProject(mouseX, view[3]-mouseY-1, z, model, proj, view, &ox, &oy, &oz);
+
+        cursor_loc = Vec(ox, oy, oz);
+
+        // Draw the cursor
+        glPushMatrix();
+        glDisable(GL_LIGHTING) ;
+        glTranslatef(ox, oy, oz);
+        glColor3ub( 255, 0, 255 );
+        glutSolidSphere(0.5,12,12);
+        glPopMatrix();
+    }
+
+    static char tmp_str[255];
+    glDisable( GL_DEPTH_TEST );
+    glLoadIdentity();
+    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+    glColor3ub( 255, 255, 0 );
+    glRasterPos2f (-0.95, -0.95);
+    sprintf( tmp_str, "control points: %d, reqularization: %2.3f, bending energy: %4.3f",
+             control_points.size(), regularization, bending_energy );
+    draw_string( tmp_str );
+    glEnable( GL_DEPTH_TEST );
+
+    glFlush();
+    glutSwapBuffers();
+    screen_dirty = false;
+#endif
 }
 
 
-void StencilVisualizer::Draw() {
+void GLApp::Callbacks::reshape( int w, int h )
+{
+    winW = w;
+    winH = h;
+    glViewport( 0, 0, winW, winH );
+    glEnable( GL_DEPTH_TEST );
+    glDisable( GL_CULL_FACE );
+    glEnable( GL_NORMALIZE );
+    glDepthFunc( GL_LESS );
+}
 
+
+void GLApp::Callbacks::idlefunc()
+{
+    glCheckErrors();
+    if (screen_dirty)
+        glutPostRedisplay();
 }
 
 
 
-void StencilVisualizer::Select() {
-
+void GLApp::Callbacks::keyboard( unsigned char key, int, int )
+{
+#if 0
+    switch (key)
+    {
+    case 'a':
+        control_points.push_back( cursor_loc );
+        calc_tps();
+        break;
+    case 'd':
+        if ( selected_cp >= 0 )
+        {
+            control_points.erase( control_points.begin() + selected_cp );
+            selected_cp = -1;
+            calc_tps();
+        }
+        break;
+        case 'c':
+        control_points.clear();
+        clear_grid();
+        break;
+        case '+':
+        regularization += 0.025;
+        calc_tps();
+        break;
+        case '-':
+        regularization -= 0.025;
+        if (regularization < 0) regularization = 0;
+        calc_tps();
+        break;
+        case '/': camZoom -= 1; break;
+        case '*': camZoom += 1; break;
+        case 'q': exit( 0 ); break;
+        }
+    screen_dirty=true;
+#endif
 }
 
-void StencilVisualizer::Deselect() {
 
+void GLApp::Callbacks::mouse( int button, int state, int, int )
+{
+#if 0
+    mouseState[ button ] = (state==GLUT_DOWN);
+    modifiers = glutGetModifiers();
+
+    glutSetCursor( GLUT_CURSOR_CROSSHAIR );
+
+    if ( button == 1 && state==GLUT_DOWN )
+        glutSetCursor( GLUT_CURSOR_CYCLE );
+
+    if ( button == 0 )
+    {
+        if ( state==GLUT_UP )
+        {
+            calc_tps();
+            screen_dirty=true;
+        }
+        else if ( state==GLUT_DOWN && selected_cp<0 )
+            keyboard( 'a', 0,0 );
+    }
+#endif
 }
 
 
-void StencilVisualizer::GetInterpolationPoints() {
 
+void GLApp::Callbacks::mouseMotion( int x, int y )
+{
+#if 0
+    if ( mouseState[0] && mouseX != -999 )
+        if ( selected_cp >= 0 )
+            control_points[selected_cp].y += -(y - mouseY)/3;
+
+    if ( mouseState[1] && mouseX != -999 )
+    {
+        camAlpha += -(y - mouseY);
+        camBeta += (x - mouseX);
+
+        screen_dirty=true;
+    }
+
+    if ( mouseX != x || mouseY != y )
+    {
+        mouseX = x;
+        mouseY = y;
+        screen_dirty=true;
+    }
+#endif
 }
 
-void StencilVisualizer::DrawInterpolation() {
 
+
+void StencilVisualizer::keyboard_special( int key, int, int )
+{
+    switch (key)
+    {
+    case GLUT_KEY_UP: camAlpha += 5 ; break;
+    case GLUT_KEY_DOWN: camAlpha += -5 ; break;
+    case GLUT_KEY_RIGHT: camBeta += -5 ; break;
+    case GLUT_KEY_LEFT: camBeta += 5 ; break;
+    }
+    screen_dirty=true;
 }
 
 
-void StencilVisualizer::GetCartesianInterpolationPoints() {
 
+void StencilVisualizer::menu_select(int mode)
+{
+    StencilVisualizer::keyboard( (unsigned char)mode, 0,0 );
 }
+
+
+void StencilVisualizer::draw_string (const char* str)
+{
+    for (unsigned i=0; i<strlen(str); i++)
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, str[i]);
+}
+
+
+
+void StencilVisualizer::create_menu(void)
+{
+    glutCreateMenu(StencilVisualizer::menu_select);
+    glutAddMenuEntry(" d     Delete control point",'d');
+    glutAddMenuEntry(" a     Add control point",'a');
+    glutAddMenuEntry(" c     Clear all",'c');
+    glutAddMenuEntry(" +     Relax more",'+');
+    glutAddMenuEntry(" -     Rela less",'-');
+    glutAddMenuEntry(" /     Zoom in",'/');
+    glutAddMenuEntry(" *     Zoom out",'*');
+    glutAddMenuEntry(" q     Exit",'q');
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
+
+void StencilVisualizer::glCheckErrors()
+{
+    GLenum errCode = glGetError();
+    if ( errCode != GL_NO_ERROR )
+    {
+        const GLubyte *errString = gluErrorString( errCode );
+        fprintf(stderr, "OpenGL error: %s\n", errString);
+    }
+}
+
+
+
+
+void StencilVisualizer::initializeWindow(int argc, char** argv) {
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
+    glutInitWindowSize( winW, winH );
+    glutInitWindowPosition( 0, 0 );
+    if ( !glutCreateWindow( "Stencil Visualizer ALPHA" ) )
+    {
+        printf( "Couldn't open window.\n" );
+        exit(EXIT_FAILURE);
+    }
+
+    glutSetCursor( GLUT_CURSOR_CROSSHAIR );
+}
+
+void StencilVisualizer::setupCallbacks() {
+
+    glutDisplayFunc( StencilVisualizer::display );
+    glutIdleFunc( StencilVisualizer::idlefunc );
+    glutMouseFunc( StencilVisualizer::mouse );
+    glutMotionFunc( StencilVisualizer::mouseMotion );
+    glutPassiveMotionFunc( StencilVisualizer::mouseMotion );
+    glutKeyboardFunc( StencilVisualizer::keyboard );
+    glutSpecialFunc( StencilVisualizer::keyboard_special );
+    glutReshapeFunc( StencilVisualizer::reshape );
+}
+
+void StencilVisualizer::setupLights() {
+    glEnable(GL_LIGHTING);
+    GLfloat lightAmbient[] = {0.5f, 0.5f, 0.5f, 1.0f} ;
+    glLightModelfv(GL_LIGHT_MODEL_TWO_SIDE, lightAmbient);
+
+    glEnable(GL_LIGHT0);
+    GLfloat light0Position[] = {0.7f, 0.5f, 0.9f, 0.0f} ;
+    GLfloat light0Ambient[]  = {0.0f, 0.5f, 1.0f, 0.8f} ;
+    GLfloat light0Diffuse[]  = {0.0f, 0.5f, 1.0f, 0.8f} ;
+    GLfloat light0Specular[] = {0.0f, 0.5f, 1.0f, 0.8f} ;
+    glLightfv(GL_LIGHT0, GL_POSITION,light0Position) ;
+    glLightfv(GL_LIGHT0, GL_AMBIENT,light0Ambient) ;
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,light0Diffuse) ;
+    glLightfv(GL_LIGHT0, GL_SPECULAR,light0Specular) ;
+
+    glEnable(GL_LIGHT1);
+    GLfloat light1Position[] = {0.5f, 0.7f, 0.2f, 0.0f} ;
+    GLfloat light1Ambient[]  = {1.0f, 0.5f, 0.0f, 0.8f} ;
+    GLfloat light1Diffuse[]  = {1.0f, 0.5f, 0.0f, 0.8f} ;
+    GLfloat light1Specular[] = {1.0f, 0.5f, 0.0f, 0.8f} ;
+    glLightfv(GL_LIGHT1, GL_POSITION,light1Position) ;
+    glLightfv(GL_LIGHT1, GL_AMBIENT,light1Ambient) ;
+    glLightfv(GL_LIGHT1, GL_DIFFUSE,light1Diffuse) ;
+    glLightfv(GL_LIGHT1, GL_SPECULAR,light1Specular) ;
+}
+#endif
--- tests/gl_stencil_visualizer/stencil_visualizer.h
+++ tests/gl_stencil_visualizer/stencil_visualizer.h
@@ -1,44 +1,117 @@
-#ifndef __STENCIL_VISUALIZER_H__
-#define __STENCIL_VISUALIZER_H__
-#include <vector> 
+#ifndef __STENCIL_VISUALIZER_CALLBACKS_H__
+#define __STENCIL_VISUALIZER_CALLBACKS_H__
+
+#include <vector>
+
 #include "Vec3.h"
+#include "rbf.h"
 
-class StencilVisualizer
-{ 
-	public: 
-		// Each StencilVisualizer is given a set of coordinates 
-		// for nodes in the stencil and the weights associated with
-		// each node. 
-		StencilVisualizer(std::vector<Vec3> nodes, std::vector<double> weights);
-		~StencilVisualizer(); 
+#include "visual_stencil.h"
 
-		// Routine that needs to be called when the GL callback 
-		// display() is called. This routine should ONLY draw
-		// the stencil sheet for a single stencil and nothing more. 
-		// Setup for the world is managed by an outside routine. 
-		void Draw(); 
+/**
+  * This class creates a glut window, loads a collection of stencils and weights as a
+  * vector of VisualStencils, and then renders these stencils using OpenGL.
+  *
+  * We have this as a separate class and not inside the main because we intend to provide
+  * this as part of the RBF library and we'd like to be able to launch multiple of instances
+  * to view a variety of test cases.
+  */
 
-		// These routines will allow us to select/deselect a stencil
-		// When selected we can draw/color the stencil differently. 
-		void Select(); 
-		void Deselect(); 
+typedef std::vector<std::vector< Vec3 > > NodeListType;
+typedef std::vector<std::vector< double > > WeightListType;
+typedef std::vector<std::vector< RBF > > BasesListType;
 
-	protected: 
-		// calculate the set of grid points that will be used
-		// to evaluate the stencil interpolant polynomial
-		virtual void GetInterpolationPoints(); 
-		
-		// draw the generated grid points and the interpolant
-		// passing through them. 
-                virtual void DrawInterpolation();
+#include <QtOpenGL/qgl.h>
+
+class StencilVisualizer : public QGLWidget
+{
+public:
+    StencilVisualizer( QWidget *parent=0, const char *name=0 ) :
+        QGLWidget(parent), shoulder(0), elbow(0) {};
+
+/**
+  * Routines related to custom behavior
+  */
+protected:
+    void wireCube(float);
 
 
-	private:	
-		// The Cartesian overlay is specific to this class
-		// We could have a GetDiskInterpolationPoints in an
-		// extending class. Both would be called by the 
-		// virtual GetInterpolationPoints() routine. 
-		void GetCartesianInterpolationPoints(); 
+private:
+    int shoulder, elbow;
+
+
+/**
+  * The following set of member functions are required for a QGLWidget
+  * to run. They are equivalent to the callbacks we would setup for
+  * GLUT.
+  */
+protected:
+    void initializeGL(void);        // Init window
+    void resizeGL( int w, int h );  // Resize window
+    void paintGL();                 // DisplayFunc
+    void keyPressEvent( QKeyEvent *e);
+
+ /*****  END REQUIRED ******/
 };
 
-#endif 	// __STENCIL_VISUALIZER_H__
+#if 0
+class StencilVisualizer
+{
+public:
+    StencilVisualizer(int argc, char** argv);
+    ~StencilVisualizer();
+
+    void setNodes(NodeListType *stencil_nodes);
+    void setWeights(WeightListType *stencil_weights);
+    void setBases(BasesListType *stencil_bases);
+
+    void setupCallbacks();
+    void setupVisualStencils();
+
+    void run();
+
+protected:
+    void setupLights();
+    void initializeWindow(int argc, char** argv);
+
+
+    void create_menu();
+
+
+    void glCheckErrors();
+    void draw_string(const char* str);
+    static void menu_select(int mode);
+
+    //TODO: routine inside this class to load visual stencil data from file
+    // void loadVisualStencils(char* node_filename, char* weight_filename, char* bases_filename);
+
+private:
+
+    static int winW, winH;
+    int mouseX, mouseY;
+    static bool mouseState[3];
+    static int modifiers;
+    Vec3 cursor_loc;
+    float camAlpha, camBeta, camZoom;
+    static bool screen_dirty;
+
+};
+
+/**
+  * A set of Callbacks used by the Visualizer application. These need to be global functions
+  * so we use a namespace to keep them organized and avoid making them static
+  */
+namespace GLApp {
+    namespace Callbacks {
+        void display();
+        void idlefunc();
+        void reshape(int w, int h);
+        void mouse(int button, int state, int, int);
+        void mouseMotion(int x, int y);
+        void keyboard(unsigned char key, int, int);
+        void keyboard_special(int key, int, int);
+    }
+}
+#endif
+
+#endif //__STENCIL_VISUALIZER_CALLBACKS_H__

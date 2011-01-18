677e47895d5c0398934cd1719dc71bcdb0bf8b84
Evan Bollig <bollig@scs.fsu.edu>
2011-01-18 05:06:16 -0500

    Forgot to add the source on that last commit. I also have two other classes which leverage this stencil class I will be adding in future commits, but I want ot have a test for each setup and ready to run before I put them into the source tree.

---
+++ src/grids/domain_decomposition/stencil.cpp
@@ -0,0 +1,205 @@
+/*
+ * File:   stencil.h
+ * Author: bollig
+ *
+ * Created on May 11, 2010, 12:48 PM
+ */
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+#include "stencil.h"
+
+//----------------------------------------------------------------------
+#if 1
+// Class originally in grid.h sorts the grid nodes by radius
+
+class ltvec {
+public:
+    static Vec3 xi;
+    static vector<Vec3>* rbf_centers;
+
+    static void setXi(Vec3& xi) {
+        ltvec::xi = xi;
+    }
+
+    static void setRbfCenters(vector<Vec3>& rbf_centers_) {
+        rbf_centers = &rbf_centers_;
+    }
+
+    bool operator()(const int i, const int j) {
+        double d1 = ((*rbf_centers)[i] - xi).square();
+        double d2 = ((*rbf_centers)[j] - xi).square();
+        // allows duplicates
+        return d1 <= d2;
+    }
+};
+
+Vec3 ltvec::xi;
+vector<Vec3>* ltvec::rbf_centers;
+
+#endif
+
+Stencil::Stencil(Grid* grid, int st_max_size, double st_max_radius) {
+    this->grid = grid;
+    this->st_max_size = st_max_size;
+    this->st_max_radius = st_max_radius;
+}
+
+Stencil::~Stencil() {
+    printf("TODO: Stencil::DESTRUCTOR\n");
+}
+
+void Stencil::setSize(double st_max_size) {
+    this->st_max_size = st_max_size;
+}
+
+void Stencil::setRadius(double st_max_radius) {
+    this->st_max_radius = st_max_radius;
+}
+
+void Stencil::generate() {
+    this->computeStencils();
+}
+
+std::vector<std::vector<int> >& Stencil::getStencils() {
+    return this->stencil_map;
+}
+
+std::vector<double>& Stencil::getAvgDist() {
+    return this->avg_stencil_radii;
+}
+
+void Stencil::computeStencils() {
+    int nb_rbf = grid->getNodeList().size();
+    int nb_bnd = grid->getBoundaryIndices().size();
+    std::vector<NodeType>& rbf_centers = grid->getNodeList();
+
+    if (st_max_size > nb_rbf) {
+        int new_stencil_size = (int) (0.5 * nb_rbf);
+        new_stencil_size = (new_stencil_size > 1) ? new_stencil_size : 1;
+        printf("\n!!!!!!!!!!!!!!!!!!!\nWARNING! Not enough centers to reach specified stencil_size (size: %d)! Using new stencil size: %d\n!!!!!!!!!!!!!!!!!!!\n\n", st_max_size, new_stencil_size);
+        st_max_size = new_stencil_size;
+    }
+
+    if (nb_bnd == 0) {
+        printf("[STENCIL] **** WARNING! nb_bnd == 0; Did the Grid::generate() execute properly?! ******\n");
+//        exit(EXIT_FAILURE);
+    }
+
+    // for each node, a vector of stencil nodes (global indexing)
+    this->stencil_map.resize(nb_rbf);
+    //printf("stencil size: %d, nb_rbf= %d\n", stencil.size(), nb_rbf);
+
+    ltvec ltvec_inst;
+    //vector<double> avg_stencil_radii;
+    vector<double> avg_bnd;
+    vector<double> avg_int;
+
+    this->avg_stencil_radii.resize(nb_rbf);
+    avg_bnd.resize(nb_bnd);
+    avg_int.resize(nb_rbf - nb_bnd);
+
+    printf("nb_rbf= %d (bnd: %d)\n", nb_rbf, nb_bnd);
+    //printf("nb_bnd= %d\n", nb_bnd);
+    //exit(0);
+
+    // O(n^2) algorithm, whose cost is independent of the number of nearest sought
+
+    for (int i = 0; i < nb_rbf; i++) {
+        Vec3& v = rbf_centers[i];
+        vector<int>& st = stencil_map[i];
+        set<int, ltvec> se;
+        ltvec::setRbfCenters(rbf_centers);
+        ltvec::setXi(v);
+
+        // find nearest points to center
+        for (int j = 0; j < nb_rbf; j++) {
+            se.insert(j);
+        }
+
+        set<int, ltvec>::iterator sei = se.begin();
+        set<int, ltvec>::iterator seii = se.begin();
+
+        // minimimum distance:
+        seii++; // I now access first point
+        double min_dist = (rbf_centers[*seii] - rbf_centers[i]).square();
+        min_dist = sqrt(min_dist);
+        //printf("min distance: %f\n", min_dist);
+
+#if 1
+        this->avg_stencil_radii[i] = 0.;
+        if (i < nb_bnd) {
+            avg_bnd[i] = 0.;
+        } else {
+            avg_int[i - nb_bnd] = 0.;
+        }
+#endif
+
+        // stencil_size = max stencil_size
+        for (int k = 0; k < st_max_size; k++) {
+            double d = (rbf_centers[*sei] - rbf_centers[i]).square();
+            double ss = sqrt(d);
+
+            // I am not sure code works yet
+            // Errors of solution to heat equation do not stay small
+            //if ((ss / min_dist) > 1.5) break;
+
+            st.push_back(*sei);
+
+#if 1
+            this->avg_stencil_radii[i] += ss;
+            if (i < nb_bnd) {
+                avg_bnd[i] += ss;
+            } else {
+                avg_int[i - nb_bnd] += ss;
+            }
+#endif
+
+            // printf("(%d, %d) dist= %f\n", i, k, ss);
+
+            // printf("el %d, d= %f\n", *sei, d);
+            sei++;
+        }
+
+#if 1
+        this->avg_stencil_radii[i] /= (st.size() - 1.); // ignore center point
+        if (i < nb_bnd) {
+            avg_bnd[i] /= (st.size() - 1.);
+        } else {
+            avg_int[i - nb_bnd] /= (st.size() - 1.);
+        }
+#endif
+
+        //printf("avg_dist[%d]= %f\n", i, avg_stencil_radii[i]);
+        //printf("nb points in stencil: %d\n", (int) st.size());
+    }
+
+#if 1
+    double avgint = 0.;
+    double avgbnd = 0.;
+
+    printf("avg_int.size() = %d\n", (int) avg_int.size());
+    printf("avg_int.size() = %d\n", (int) avg_bnd.size());
+
+    if (nb_rbf - nb_bnd > 0) {
+        for (int i = 0; i < avg_int.size(); i++) {
+            avgint += avg_int[i];
+        }
+        avgint /= avg_int.size();
+    } else {
+        avgint = 0.;
+    }
+
+    // There should always be boundary point(s)
+    for (int i = 0; i < avg_bnd.size(); i++) {
+        avgbnd += avg_bnd[i];
+    }
+    avgbnd /= avg_bnd.size();
+
+    printf("mean of mean interior distances: %f (size: %d)\n", avgint, (int) avg_int.size());
+    printf("mean of mean boundary distances: %f (size: %d)\n", avgbnd, (int) avg_bnd.size());
+#endif
+}
+//----------------------------------------------------------------------

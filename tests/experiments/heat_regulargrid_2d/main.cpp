0d36e603529d917666af54c859d703b569c2ecb0
Evan Bollig <bollig@scs.fsu.edu>
2011-01-26 04:35:38 -0500

    Well, its definitely conditioning related. Heres the problem: for those test cases when my weights from the GPU dont match with teh CPU its becasue the epsilon pushes the weights to something so small that we need double precision to capture them. The CPU has this luxury whereas the GPU does not. IF i switch #if 0 to #if 1 in Derivative::computeDerivativesCPU as I am doing here (in this commit), then the CPU and GPU compute derivatives using single precision and I get exactly the same result output.
    If I keep the CPU at double precision this appears to be just one more test to run before accepting that the choice of epsilon is appropriate.

--- tests/experiments/heat_regulargrid_2d/main.cpp
+++ tests/experiments/heat_regulargrid_2d/main.cpp
@@ -227,17 +227,21 @@ int main(int argc, char** argv) {
         }
 
 
-		if ( (fabs(xderiv_gpu[i] - xderiv_cpu[i]) > 9e-5) 
-				|| (fabs(yderiv_gpu[i] - yderiv_cpu[i]) > 9e-5) 
-				|| (fabs(zderiv_gpu[i] - zderiv_cpu[i]) > 9e-5) 
-				|| (fabs(lderiv_gpu[i] - lderiv_cpu[i]) > 9e-5))
+		if ( (fabs(xderiv_gpu[i] - xderiv_cpu[i]) > 1e-5) 
+				|| (fabs(yderiv_gpu[i] - yderiv_cpu[i]) > 1e-5) 
+				|| (fabs(zderiv_gpu[i] - zderiv_cpu[i]) > 1e-5) 
+				|| (fabs(lderiv_gpu[i] - lderiv_cpu[i]) > 1e-5))
 		{
 			std::cout << "WARNING! SINGLE PRECISION GPU COULD NOT CALCULATE DERIVATIVE WELL ENOUGH!\n";
 			std::cout << "Test failed on " << i << std::endl;
-			std::cout << "X: " << xderiv_gpu[i] - xderiv_cpu[i] << std:: endl; 
-			std::cout << "Y: " << yderiv_gpu[i] - yderiv_cpu[i] << std:: endl; 
-			std::cout << "Z: " << zderiv_gpu[i] - zderiv_cpu[i] << std:: endl; 
-			std::cout << "LAPL: " << lderiv_gpu[i] - lderiv_cpu[i] << std:: endl; 
+			std::cout << "X: " << xderiv_gpu[i] - xderiv_cpu[i] << " \t \t"; 
+			std::cout << "GPU: " << xderiv_gpu[i] << " CPU: " << xderiv_cpu[i] << std:: endl; 
+			std::cout << "Y: " << yderiv_gpu[i] - yderiv_cpu[i] << " \t \t"; 
+			std::cout << "GPU: " << yderiv_gpu[i] << " CPU: " << yderiv_cpu[i] << std:: endl; 
+			std::cout << "Z: " << zderiv_gpu[i] - zderiv_cpu[i] << " \t \t"; 
+			std::cout << "GPU: " << zderiv_gpu[i] << " CPU: " << zderiv_cpu[i] << std:: endl; 
+			std::cout << "LAPL: " << lderiv_gpu[i] - lderiv_cpu[i] << " \t \t"; 
+			std::cout << "GPU: " << lderiv_gpu[i] << " CPU: " << lderiv_cpu[i] << std:: endl; 
 			exit(EXIT_FAILURE); 
 		}
 	}

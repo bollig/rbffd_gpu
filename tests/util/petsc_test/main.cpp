95dea3343207cf57735b570b0ac06f1d4104301d
Evan Bollig <bollig@gmail.com>
2011-02-22 04:12:45 -0500

    Added a test that shows how to link in petsc. However, I need to add a PETSC call within the main.cpp to verify I can actually use the library in my source. Eventually I want to do a parallel eigenvalue decomposition. Optionally I can also integrate my RBF-FD into the work that Knepley did with compact collocation and restrictive additive schwarz. I think I found his source for the paper and am trying to understand what he was doing in it.

---
+++ tests/util/petsc_test/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+// GLOBAL VARIABLES
+
+//----------------------------------------------------------------------
+int main (int argc, char** argv)
+{
+	if (argc > 1) {
+		printf("ARGC = %d\n", argc); 
+		return EXIT_FAILURE; 	// FAIL TEST
+	} 
+
+	return EXIT_SUCCESS; 		// PASS TEST
+}
+//----------------------------------------------------------------------

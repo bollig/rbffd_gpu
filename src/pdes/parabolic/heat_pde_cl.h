bdcb0d41507d1f1f88f5656ed6e42e3ed045fd28
Evan Bollig <bollig@gmail.com>
2011-05-13 18:22:52 -0400

    Added another set of missing files

---
+++ src/pdes/parabolic/heat_pde_cl.h
@@ -0,0 +1,14 @@
+#ifndef __HEAT_PDE_CL_H__
+#define __HEAT_PDE_CL_H__
+
+#include "pdes/time_dependent_pde_cl.h"
+#include "pdes/parabolic/heat_pde.h"
+
+class HeatPDE_CL : TimeDependentPDE_CL
+{
+    public: 
+        virtual void advance(); 
+
+}; 
+#endif 
+

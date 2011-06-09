f613f40effb07ecb9d8381fac4dbd7246daa4a66
Evan Bollig <bollig@scs.fsu.edu>
2011-06-09 06:06:37 -0400

    Added a 1D test with known exact solution to verify that I am solving the diffusion equation correctly. It works. Although stability is an issue, for 1000 nodes, stencil size 3, I can take a step of 2e-5. The recommended step for a FD scheme (0.5*dx^2/k) is 5e-6. This is good news. Some RBF methods are capable of larger steps. I want to sleep a little, then Ill put in a new 2D exact solution verification test to make sure we compute just as well in 2D. Also, this case fails the derivative tests...too few stencil nodes and poor choice of epsilon, but it gets a solution for the problem ~1e-6 rel error at t=1
    I think the problem I have had this week, now that weights and grid and
    subdomains are
    generated properly is that my exact solution is not "exact" for the heat
    equation. In otherwords, we're solving the problem correctly but not
    getting what the exact solution expects

---
+++ tests/experiments/heat_1d_verification/exact_1D.h
@@ -0,0 +1,71 @@
+#ifndef _EXACT_1D_H_
+#define _EXACT_1D_H_
+
+#include "exact_solutions/exact_solution.h"
+
+// These expressions were obtained from Mathematica. 
+// The original idea for this solution came from
+// http://web.cecs.pdx.edu/~gerry/class/ME448/codes/FDheat.pdf
+class Exact1D : public ExactSolution
+{
+    private:
+        double decay; 
+        double L;
+    public:
+        Exact1D(double maxX, double alpha)
+            : ExactSolution(1), 
+            L(maxX), decay(alpha)
+    {;}
+        ~Exact1D();
+
+        virtual double operator()(double x, double y, double z, double t) {
+            double alpha = this->diffuseCoefficient(x,y,z,0.,t); 
+            double val = sin((M_PI * x)/L) * exp(-(alpha * M_PI * M_PI * t)/(L*L)); 
+            return val;
+        }
+        virtual double laplacian(double x, double y, double z, double t) {
+            double alpha = this->diffuseCoefficient(x,y,z,0.,t); 
+            double val = -((M_PI * M_PI * sin((M_PI*x)/L)) * exp(-(M_PI * M_PI * t * alpha)/(L*L))) / (L*L);
+            return val;
+        }
+
+        virtual double xderiv(double x, double y, double z, double t) {
+            double alpha = this->diffuseCoefficient(x,y,z,0.,t); 
+            double val = M_PI * cos((M_PI * x)/L) * ( L *exp( - M_PI * M_PI * t * alpha)/(L*L));
+            return val;
+        }
+        virtual double yderiv(double x, double y, double z, double t) {
+            return 0.;
+        }
+        virtual double zderiv(double x, double y, double z, double t) {
+            return 0.;
+        }
+
+        virtual double tderiv(double x, double y, double z, double t) {
+            double alpha = this->diffuseCoefficient(x,y,z,0.,t); 
+            double val = -(((M_PI*M_PI)*alpha * sin((M_PI*x)/L)) * exp((M_PI*M_PI*t*alpha)/(L*L))) / (L*L);
+            return val; 
+        }
+
+        // Return the diffusivity at node v (K=1 by default)
+        virtual double diffuseCoefficient(double x, double y, double z, double sol, double t) {
+            return decay;            
+        }
+        virtual double diffuse_xderiv(double x, double y, double z, double sol, double t) {
+            return 0.;
+        }
+        virtual double diffuse_yderiv(double x, double y, double z, double sol, double t) {
+            return 0.;
+        }
+        virtual double diffuse_zderiv(double x, double y, double z, double sol, double t) {
+            return 0.;
+        }
+        virtual double diffuse_tderiv(double x, double y, double z, double sol, double t) {
+            return 0.;
+        }
+};
+//----------------------------------------------------------------------
+
+#endif 
+
+

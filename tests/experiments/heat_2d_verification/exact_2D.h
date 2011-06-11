e9caa81fa0b9ef22b0d74398abd612b2bcb6f34d
Evan Bollig <bollig@scs.fsu.edu>
2011-06-11 18:19:43 -0400

    Fixed problem with laplacian in 2D.

--- tests/experiments/heat_2d_verification/exact_2D.h
+++ tests/experiments/heat_2d_verification/exact_2D.h
@@ -24,7 +24,7 @@ class Exact2D : public ExactSolution
         Exact2D(double minX, double maxX, double minY, double maxY, double alpha)
             // 2D
             : ExactSolution(2), 
-            n(8),m(5),B(2.),
+            n(8),m(5),B(1.),
             xmin(minX), ymin(minY),
             L(maxX-minX),H(maxY-minY),
             diffuseConst(alpha)
@@ -47,13 +47,15 @@ class Exact2D : public ExactSolution
         virtual double laplacian(double x, double y, double z, double t) {
             double alpha = this->diffuseCoefficient(x,y,z,0.,t); 
             double initialConst = B; 
-            double d2_spatial_xx = -sin((n * M_PI * (x-xmin))/L) * (n*n*M_PI*M_PI) / L*L;
-            double d2_spatial_yy = -sin((m * M_PI * (y-ymin))/H) * (m*m*M_PI*M_PI) / H*H;
+            double spatial_x = sin((n * M_PI * (x-xmin))/L);
+            double spatial_y = sin((m * M_PI * (y-ymin))/H);
+            double d2_spatial_xx = -sin((n * M_PI * (x-xmin))/L) * spatial_y * (n*n*M_PI*M_PI) / L*L;
+            double d2_spatial_yy = -sin((m * M_PI * (y-ymin))/H) * spatial_x * (m*m*M_PI*M_PI) / H*H;
             double lambda = ((n * n * M_PI * M_PI)/(L*L)) + ((m * m * M_PI * M_PI)/(H*H));
             double temporal = exp(-alpha * t * lambda);
 
             //changed from operator()
-            double val = initialConst * d2_spatial_xx * d2_spatial_yy * temporal;
+            double val = initialConst * (d2_spatial_xx + d2_spatial_xx) * temporal;
             
             return val;
         }

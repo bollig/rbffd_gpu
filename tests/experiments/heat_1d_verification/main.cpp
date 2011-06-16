d6522bd4cff91afe0529b6b08ac05fc3af6c50b3
Evan Bollig <bollig@scs.fsu.edu>
2011-06-16 16:48:10 -0600

    disabled gpu heat in the 1d verficiation

--- tests/experiments/heat_1d_verification/main.cpp
+++ tests/experiments/heat_1d_verification/main.cpp
@@ -307,12 +307,15 @@ int main(int argc, char** argv) {
     TimeDependentPDE* pde; 
     tm["heat_init"]->start(); 
     // We need to provide comm_unit to pass ghost node info
-#if 1
+#if 0
     if (use_gpu) {
         pde = new HeatPDE_CL(subdomain, (RBFFD_CL*)der, comm_unit, uniformDiffusion, true); 
     } else 
 #endif
     { 
+        if (use_gpu) {
+            std::cout << "GPU version of HeatPDE disabled.\n";
+        }
         // Implies initial conditions are generated
         // true here indicates the weights are computed. 
         pde = new HeatPDE(subdomain, der, comm_unit, uniformDiffusion, true);

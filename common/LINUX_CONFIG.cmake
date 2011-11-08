ebc21609aa7bfcfc79f1f409d7443f899ba51658
Evan Bollig <efb06@kidlogin1.nics.utk.edu>
2011-11-08 16:45:38 -0500

    Doing the aligne memory

--- common/LINUX_CONFIG.cmake
+++ common/LINUX_CONFIG.cmake
@@ -3,12 +3,13 @@ MESSAGE(STATUS "LINUX detected.")
 get_filename_component(_compiler ${CMAKE_CXX_COMPILER} NAME)
 
 IF (${_compiler} STREQUAL "icpc")
-    add_definitions(-opt-report -par-report -vec-report ) 
+    add_definitions(-vec-report ) #-opt-report -par-report -vec-report ) 
     SET(CUDA_NVCC_FLAGS "-ccbin;icc;-arch=sm_13")
-ELSE ()
+ELSE (${_compiler} STREQUAL "icpc")
     # ASSUME WE'RE USING GCC
-    add_definitions(-ftree-vectorizer-verbose=2 ) 
-    set(USE_ICC OFF)
+    # auto-vectorize was added to -O3 in 2008. The old way of adding it was with the ftree-vectorize
+    #  add_definitions(-ftree-vectorizer-verbose=2 -ftree-vectorize ) 
+    add_definitions(-ftree-vectorize ) 
 
     SITE_NAME(_hostname)
     if (${_hostname} MATCHES "kid.*")
@@ -21,7 +22,7 @@ ELSE ()
     	
     SET(CUDA_NVCC_FLAGS "-ccbin;g++;-arch=sm_13")
 
-ENDIF ()
+ENDIF (${_compiler} STREQUAL "icpc")
 
 
 IF (CUDA_FOUND) 

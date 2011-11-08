fb8579cea0d57dcf01b1a08b80c4d40dbbc7d9f5
Evan Bollig <efb06@kidlogin1.nics.utk.edu>
2011-11-08 15:57:01 -0500

    SPecify the compiler with USE_GCC or USE_ICC. Easily switches between compilers on keeneland. Or dont specify the compiler and Cmake will choose for us.

--- common/LINUX_CONFIG.cmake
+++ common/LINUX_CONFIG.cmake
@@ -1,34 +1,34 @@
 MESSAGE(STATUS "LINUX detected.")
 
+get_filename_component(_compiler ${CMAKE_CXX_COMPILER} NAME)
+
+IF (${_compiler} STREQUAL "icpc")
+    add_definitions(-opt-report -par-report -vec-report ) 
+    SET(CUDA_NVCC_FLAGS "-ccbin;icc;-arch=sm_13")
+ELSE ()
+    # ASSUME WE'RE USING GCC
+    add_definitions(-ftree-vectorizer-verbose=2 ) 
+    set(USE_ICC OFF)
+
+    SITE_NAME(_hostname)
+    if (${_hostname} MATCHES "kid.*")
+        FIND_LIBRARY (iomp5 iomp5 PATHS
+            /opt/intel/composerxe-2011.5.220/compiler/lib/intel64/
+            )
+        MESSAGE(STATUS "Found iomp5 in: ${iomp5}")
+        set (ADDITIONAL_REQUIRED_LIBRARIES "${ADDITIONAL_REQUIRED_LIBRARIES};${iomp5}")
+    ENDIF(${_hostname} MATCHES "kid.*")
+    	
+    SET(CUDA_NVCC_FLAGS "-ccbin;g++;-arch=sm_13")
+
+ENDIF ()
+
+
 IF (CUDA_FOUND) 
-    get_filename_component(_compiler ${CMAKE_CXX_COMPILER} NAME)
-    IF (${_compiler} STREQUAL "icpc")
-        add_definitions(-opt-report -par-report -vec-report ) 
-    	SET(CUDA_NVCC_FLAGS "-ccbin;icc;-arch=sm_13")
-    ELSE ()
-        # ASSUME WE'RE USING GCC
-        SITE_NAME(_hostname)
-        if (${_hostname} MATCHES "kid.*")
-            add_definitions(-ftree-vectorizer-verbose=2 ) 
-            # We are on keeneland and need this library
-            SET (ADDITIONAL_REQUIRED_LIBRARIES 
-                #    /opt/intel/composerxe-2011.5.220/compiler/lib/intel64/libiomp5.so
-                iomp5
-            )
-            SET (ICC_LINK_DIR
-                /opt/intel/composerxe-2011.5.220/compiler/lib/intel64/
-            )
-        ENDIF(${_hostname} MATCHES "kid.*")
-    ENDIF ()
-    IF (USE_ICC) 
-    	SET(CUDA_NVCC_FLAGS "-ccbin;icc;-arch=sm_13")
-    ELSE (USE_ICC)
-    	SET(CUDA_NVCC_FLAGS "-ccbin;g++;-arch=sm_13")
-    ENDIF (USE_ICC)
-    #	SET(CUDA_NVCC_FLAGS "-arch=sm_13")
-	SET(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS};-Xcompiler;-D__builtin_stdarg_start=__builtin_va_start")
+    SET(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS};-Xcompiler;-D__builtin_stdarg_start=__builtin_va_start")
 	MESSAGE(STATUS "CUDA NVCCFLAGS SET: ${CUDA_NVCC_FLAGS}")
 ENDIF (CUDA_FOUND)
+
 
 # One or more dirs split by spaces. This is a command so it can be called multiple times
 INCLUDE_DIRECTORIES (
@@ -45,7 +45,7 @@ LINK_DIRECTORIES (
 # Additional libraries required by this OS
 # NOTE: order of libraries is important in Linux. 
 # 	does not matter on macOSX
-SET (ADDITIONAL_REQUIRED_LIBRARIES 
+SET (ADDITIONAL_REQUIRED_LIBRARIES
     ${ADDITIONAL_REQUIRED_LIBRARIES}
 )
 

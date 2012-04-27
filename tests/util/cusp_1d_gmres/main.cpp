78790bf44a68c492b555609b4f1d5e65b4098bb1
Evan Bollig <bollig@gmail.com>
2012-04-27 15:11:45 -0400

    Need to transfer to troi to test cuda

---
+++ tests/util/cusp_1d_gmres/main.cpp
@@ -0,0 +1,371 @@
+#include <stdlib.h>
+#include <map> 
+
+#include "grids/regulargrid.h"
+
+#include "grids/domain.h"
+#include "rbffd/derivative_tests.h"
+#include "rbffd/rbffd.h"
+#include "rbffd/rbffd_cl.h"
+
+#include "timer_eb.h"
+#include "utils/comm/communicator.h"
+#include "utils/io/pde_writer.h"
+
+#if USE_VTK
+#include "utils/io/vtu_pde_writer.h"
+#endif 
+
+#include "poisson_1d_cu.h"
+
+using namespace std;
+using namespace EB;
+
+//----------------------------------------------------------------------
+//NOTE: EVERYTHING BELOW IN MAIN WAS COPIED FROM heat_regulargrid_2d/main.cpp
+//----------------------------------------------------------------------
+
+int main(int argc, char** argv) {
+    TimerList tm;
+
+    tm["total"] = new Timer("[Main] Total runtime for this proc");
+    tm["grid"] = new Timer("[Main] Grid generation");
+    tm["stencils"] = new Timer("[Main] Stencil generation");
+    tm["settings"] = new Timer("[Main] Load settings"); 
+    tm["decompose"] = new Timer("[Main] Decompose domain"); 
+    tm["consolidate"] = new Timer("[Main] Consolidate subdomain solutions"); 
+    tm["updates"] = new Timer("[Main] Broadcast solution updates"); 
+    tm["send"] = new Timer("[Main] Send subdomains to other processors (master only)"); 
+    tm["receive"] = new Timer("[Main] Receive subdomain from master (clients only)"); 
+    tm["weights"] = new Timer("[Main] Compute all stencils weights"); 
+    tm["oneWeight"] = new Timer("[Main] Compute single stencil weights"); 
+
+    // grid should only be valid instance for MASTER
+    Grid* grid=NULL; 
+    Domain* subdomain; 
+
+    int dim = 1; 
+
+    tm["total"]->start(); 
+
+    Communicator* comm_unit = new Communicator(argc, argv);
+
+    cout << " Got Rank: " << comm_unit->getRank() << endl;
+    cout << " Got Size: " << comm_unit->getSize() << endl;
+
+    tm["settings"]->start(); 
+
+    ProjectSettings* settings = new ProjectSettings(argc, argv, comm_unit->getRank());
+
+    if (comm_unit->isMaster()) {
+
+        int ns_nx = settings->GetSettingAs<int>("NS_NB_X", ProjectSettings::optional, "10"); 
+        int ns_ny = settings->GetSettingAs<int>("NS_NB_Y", ProjectSettings::optional, "10");
+        int ns_nz = settings->GetSettingAs<int>("NS_NB_Z", ProjectSettings::optional, "10");
+
+        int stencil_size = settings->GetSettingAs<int>("STENCIL_SIZE", ProjectSettings::required); 
+
+        int N = settings->GetSettingAs<int>("NB_X", ProjectSettings::optional, "10"); 
+        double minX = settings->GetSettingAs<double>("MIN_X", ProjectSettings::optional, "0"); 
+        double maxX = settings->GetSettingAs<double>("MAX_X", ProjectSettings::optional, "1"); 
+
+        tm["settings"]->stop(); 
+
+        grid = new RegularGrid(N, minX, maxX); 
+
+        grid->setMaxStencilSize(stencil_size); 
+
+        Grid::GridLoadErrType err = grid->loadFromFile(); 
+        if (err == Grid::NO_GRID_FILES) 
+        {
+            printf("\n************** Generating new Grid **************\n"); 
+            grid->setSortBoundaryNodes(true); 
+            tm["grid"]->start(); 
+            grid->generate();
+            tm["grid"]->stop(); 
+            grid->writeToFile(); 
+        } 
+        if ((err == Grid::NO_GRID_FILES) || (err == Grid::NO_STENCIL_FILES)) {
+            std::cout << "Generating stencils files\n";
+            tm["stencils"]->start(); 
+            //            grid->setNSHashDims(ns_nx, ns_ny, ns_nz);
+            //            grid->generateStencils(Grid::ST_HASH);   
+            //            grid->generateStencils(Grid::ST_BRUTE_FORCE);  
+            grid->generateStencils(Grid::ST_KDTREE);   
+            tm["stencils"]->stop();
+            grid->writeToFile(); 
+            tm.writeToFile("gridgen_timer_log"); 
+        }
+
+        int x_subdivisions = comm_unit->getSize();		// reduce this to impact y dimension as well 
+        int y_subdivisions = (comm_unit->getSize() - x_subdivisions) + 1; 
+
+        // TODO: load subdomain from disk
+
+        // Construct a new domain given a grid. 
+        Domain* original_domain = new Domain(dim, grid, comm_unit->getSize()); 
+        // pre allocate pointers to all of the subdivisions
+        std::vector<Domain*> subdomain_list(x_subdivisions*y_subdivisions);
+        // allocate and fill in details on subdivisions
+
+        std::cout << "Generating subdomains\n";
+        tm["decompose"]->start();
+        //original_domain->printVerboseDependencyGraph();
+        original_domain->generateDecomposition(subdomain_list, x_subdivisions, y_subdivisions); 
+        tm["decompose"]->stop();
+
+        tm["send"]->start(); 
+        subdomain = subdomain_list[0]; 
+        for (int i = 1; i < comm_unit->getSize(); i++) {
+            std::cout << "Sending subdomain[" << i << "]\n";
+            comm_unit->sendObject(subdomain_list[i], i); 
+
+            // Now that its sent, we can free the memory for domains on other processors.
+            delete(subdomain_list[i]);
+        }
+        tm["send"]->stop(); 
+
+        printf("----------------------\nEND MASTER ONLY\n----------------------\n\n\n");
+
+    } else {
+        tm["settings"]->stop(); 
+        cout << "MPI RANK " << comm_unit->getRank() << ": waiting to receive subdomain"
+            << endl;
+
+        tm["receive"]->start(); 
+        subdomain = new Domain(); // EMPTY object that will be filled by MPI
+        comm_unit->receiveObject(subdomain, 0); // Receive from CPU (0)
+        subdomain->setCommSize(comm_unit->getSize());
+        tm["receive"]->stop(); 
+        //subdomain->writeToFile();
+    }
+
+    comm_unit->barrier();
+
+    RBFFD* der = new RBFFD(RBFFD::LAPL | RBFFD::X | RBFFD::Y | RBFFD::Z, subdomain, dim, comm_unit->getRank()); 
+
+    int use_var_eps = settings->GetSettingAs<int>("USE_VAR_EPSILON", ProjectSettings::optional, "0");
+    if (use_var_eps) {
+        double alpha = settings->GetSettingAs<double>("VAR_EPSILON_ALPHA", ProjectSettings::optional, "1.0"); 
+        double beta = settings->GetSettingAs<double>("VAR_EPSILON_BETA", ProjectSettings::optional, "1.0"); 
+        //der->setVariableEpsilon(subdomain->getStencilRadii(), subdomain->getStencils(), alpha, beta); 
+        der->setVariableEpsilon(alpha, beta); 
+    } else {
+        double epsilon = settings->GetSettingAs<double>("EPSILON", ProjectSettings::required);
+        der->setEpsilon(epsilon);
+    }
+
+    // Try loading all the weight files
+    int err = der->loadFromFile(RBFFD::X); 
+    err += der->loadFromFile(RBFFD::Y); 
+    err += der->loadFromFile(RBFFD::Z); 
+    err += der->loadFromFile(RBFFD::LAPL); 
+
+    if (err) { 
+        printf("start computing weights\n");
+        tm["weights"]->start(); 
+
+        // NOTE: good test for Direct vs Contour
+        // Grid 11x11, vareps=0.05; Look at stencil 12. SHould have -100, 25,
+        // 25, 25, 25 (i.e., -4,1,1,1,1) not sure why scaling is off.
+        der->computeAllWeightsForAllStencils();
+        tm["weights"]->stop(); 
+
+        cout << "end computing weights" << endl;
+
+        der->writeToFile(RBFFD::X);
+        der->writeToFile(RBFFD::Y);
+        der->writeToFile(RBFFD::Z);
+        der->writeToFile(RBFFD::LAPL);
+
+        cout << "end write weights to file" << endl;
+    }
+
+    Poisson1D_PDE_VCL* pde = new Poisson1D_PDE_VCL(subdomain, der, comm_unit); 
+
+    pde->assemble();
+    pde->write_System(); 
+    pde->solve();
+    pde->write_Solution(); 
+    
+#if 0
+    // Broadcast updates for timestep, initial conditions for ghost nodes, etc. 
+    tm["updates"]->start(); 
+    comm_unit->broadcastObjectUpdates(pde);
+    comm_unit->barrier();
+    tm["updates"]->stop();
+
+    tm["heat_init"]->stop(); 
+
+    //TODO:    pde->setRelErrTol(max_global_rel_error); 
+
+    // Setup a logging class that will monitor our iteration and dump intermediate files
+#if USE_VTK
+    // TODO: update VtuPDEWriter for the new PDE classes
+    PDEWriter* writer = new VtuPDEWriter(subdomain, pde, comm_unit, local_sol_dump_frequency, global_sol_dump_frequency);
+#else 
+    PDEWriter* writer = new PDEWriter(subdomain, pde, comm_unit, local_sol_dump_frequency, global_sol_dump_frequency);
+#endif 
+
+    // Test DT: 
+    // 1) get the minimum avg stencil radius (for stencil area--i.e., dx^2)
+    double avgdx = 1000.;
+    std::vector<StencilType>& sten = subdomain->getStencils();
+    for (size_t i=0; i < sten.size(); i++) {
+        double dx = subdomain->getStencilRadius(i);
+        if (dx < avgdx) {
+            avgdx = dx; 
+        }
+    }
+    // Laplacian = d^2/dx^2
+    double sten_area = avgdx*avgdx;
+
+    double max_dt = (0.5*sten_area)/ddecay;
+
+    // Not sure where Gordon came up with this parameter.
+    // for second centered difference and euler time we have nu = 0.5
+    //          dt <= nu/dx^2 
+    // is valid for stability in some FD schemes. 
+    // double max_dt = 0.2*(sten_area);
+    printf("dt = %f (FD suggested max_dt(0.5*dx^2/K)= %f; 0.5dx^2 = %f)\n", dt, max_dt, 0.5*sten_area);
+    // This appears to be consistent with Chinchipatnam2006 (Thesis)
+    // TODO: get more details on CFL for RBFFD
+    // note: checking stability only works if we have all weights for all
+    // nodes, so we dont do it in parallel
+    if (compute_eigenvalues && (comm_unit->getSize() == 1)) {
+        RBFFD::EigenvalueOutput eigs = der->getEigenvalues();
+        // Not sure why this is 2
+        max_dt = 2. / eigs.max_neg_eig;
+        printf("Suggested max_dt based on eigenvalues (2/lambda_max)= %f\n", max_dt);
+
+        // CFL condition:
+        if (dt > max_dt) {
+            std::cout << "WARNING! your choice of timestep (" << dt << ") is TOO LARGE for to maintain stability of system. According to eigenvalues, it must be less than " << max_dt << std::endl;
+            if (use_eigen_dt) {
+                dt = max_dt;
+            } else {
+                //exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    std::cout << "[MAIN] ********* USING TIMESTEP dt=" << dt << " ********** " << std::endl;
+
+    //    subdomain->printCenterMemberships(subdomain->G, "G = " );
+    //subdomain->printBoundaryIndices("INDICES OF GLOBAL BOUNDARY NODES: ");
+    int iter;
+
+    int num_iters = (int) ((end_time - start_time) / dt);
+    std::cout << "NUM_ITERS = " << num_iters << std::endl;
+
+    for (iter = 0; iter < num_iters && iter < max_num_iters; iter++) {
+        writer->update(iter);
+
+#if 0
+        char label[256]; 
+        sprintf(label, "LOCAL INPUT SOLUTION [local_indx (global_indx)] FOR ITERATION %d", iter); 
+        pde->printSolution(label); 
+#endif 
+
+        tm["timestep"]->start(); 
+        pde->advance((TimeDependentPDE::TimeScheme)timescheme, dt);
+        tm["timestep"]->stop(); 
+
+        // This just double checks that all procs have ghost node info.
+        // pde->advance(..) should broadcast intermediate updates as needed,
+        // but updated solution. 
+        tm["updates"]->start(); 
+        comm_unit->broadcastObjectUpdates(pde);
+        comm_unit->barrier();
+        tm["updates"]->stop();
+
+        if (!(iter % local_sol_dump_frequency)) {
+
+            std::cout << "\n*********** Rank " << comm_unit->getRank() << " Local Solution [ Iteration: " << iter << " (t = " << pde->getTime() << ") ] *************" << endl;
+            pde->checkLocalError(exact, max_local_rel_error); 
+        }
+        if (!(iter % global_sol_dump_frequency)) {
+            tm["consolidate"]->start(); 
+            comm_unit->consolidateObjects(pde);
+            comm_unit->barrier();
+            tm["consolidate"]->stop(); 
+            if (comm_unit->isMaster()) {
+                std::cout << "\n*********** Global Solution [ Iteration: " << iter << " (t = " << pde->getTime() << ") ] *************" << endl;
+                pde->checkGlobalError(exact, grid, max_global_rel_error); 
+            }
+        }
+#if 0
+        sprintf(label, "LOCAL UPDATED SOLUTION [local_indx (global_indx)] AFTER %d ITERATIONS", iter+1); 
+        pde->printSolution(label); 
+#endif 
+
+        //        double nrm = pde->maxNorm();
+        if (prompt_to_continue && comm_unit->isMaster()) {
+            std::string buf; 
+            cout << "Press [Enter] to continue" << std::endl;
+            cin.get(); 
+        }
+    }
+#if 1
+    printf("after heat\n");
+    std::cout << "\n*********** Rank " << comm_unit->getRank() << " Final Local Solution [ Iteration: " << iter << " (t = " << pde->getTime() << ") ] *************" << endl;
+    pde->checkLocalError(exact, max_local_rel_error); 
+
+    // NOTE: all local subdomains have a U_G solution which is consolidated
+    // into the MASTER process "global_U_G" solution. 
+    tm["consolidate"]->start(); 
+    comm_unit->consolidateObjects(pde);
+    comm_unit->barrier();
+    tm["consolidate"]->stop(); 
+    //    subdomain->writeGlobalSolutionToFile(-1); 
+    std::cout << "Checking Solution on Master\n";
+    if (comm_unit->getRank() == 0) {
+        pde->writeGlobalGridAndSolutionToFile(grid->getNodeList(), (char*) "FINAL_SOLUTION.txt");
+#if 0
+        // NOTE: the final solution is assembled, but we have to use the 
+        // GLOBAL node list instead of a local subdomain node list
+        cout << "FINAL ITER: " << iter << endl;
+        std::vector<double> final_sol(grid->getNodeListSize()); 
+        ifstream fin; 
+        fin.open("FINAL_SOLUTION.txt"); 
+
+        int count = 0; 
+        for (int count = 0; count < final_sol.size(); count++) {
+            Vec3 node; 
+            double val;
+            fin >> node[0] >> node[1] >> node[2] >> val;
+            if (fin.good()) {
+                final_sol[count] = val;
+                // std::cout << "Read: " << node << ", " << final_sol[count] << std::endl; 
+            }
+        }
+        fin.close();
+#endif 
+        std::cout << "============== Verifying Accuracy of Final Solution =============\n"; 
+        pde->checkGlobalError(exact, grid, max_global_rel_error); 
+        std::cout << "============== Solution Valid =============\n"; 
+
+        delete(grid);
+    }
+#endif 
+
+    cout.flush();
+    printf("Cleaning up objects\n");
+
+    // Writer first so we can dump final solution
+    delete(writer);
+    delete(pde);
+#endif 
+    delete(subdomain);
+    delete(settings);
+    delete(comm_unit); 
+
+    tm["total"]->stop();
+    tm["total"]->printAll(); 
+    tm["total"]->writeAllToFile();
+
+    printf("\n\nREACHED THE END OF MAIN\n\n");
+
+    return 0;
+}
+//----------------------------------------------------------------------
---
+++ tests/util/cusp_1d_gmres/poisson_1d_cu.h
@@ -0,0 +1,312 @@
+#ifndef __POISSON1D_CU_H__
+#define __POISSON1D_CU_H__
+
+#include "manufactured_solution.h"
+#include "implicit_pde.h"
+
+#include <cusp/hyb_matrix.h>
+#include <cusp/ell_matrix.h>
+#include <cusp/csr_matrix.h>
+#include <cusp/coo_matrix.h>
+#include <cusp/monitor.h>
+#include <cusp/krylov/cg.h>
+#include <cusp/krylov/gmres.h>
+#include <cusp/gallery/poisson.h>
+#include <cusp/print.h>
+#include <cusp/array2d.h>
+#include <cusp/multiply.h>
+#include <cusp/blas.h>
+#include <cusp/io/matrix_market.h>
+#include <cusp/precond/diagonal.h>
+#include <cusp/precond/ainv.h>
+#include <cusp/precond/smoothed_aggregation.h>
+#include <cusp/precond/aggregate.h>
+#include <cusp/precond/smooth.h>
+#include <cusp/precond/strength.h>
+
+#include <thrust/host_vector.h> 
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+
+class Poisson1D_PDE_DEV : public ImplicitPDE
+{
+
+    typedef cusp::csr_matrix<double, cusp::host_memory> HOST_MAT_t; 
+    typedef boost::numeric::ublas::vector<double> HOST_VEC_t; 
+    typedef viennacl::compressed_matrix<double> DEV_MAT_t; 
+    typedef viennacl::vector<double> DEV_VEC_t; 
+
+
+    protected:
+    HOST_MAT_t* LHS_host; 
+    HOST_VEC_t* RHS_host; 
+    HOST_VEC_t* U_exact_host; 
+    HOST_VEC_t* U_approx_host; 
+
+    DEV_MAT_t* LHS_dev; 
+    DEV_VEC_t* RHS_dev; 
+    DEV_VEC_t* U_exact_dev; 
+    DEV_VEC_t* U_approx_dev; 
+
+
+    // Problem size (Num Rows)
+    unsigned int N;
+    // Problem size (Num Cols)
+    unsigned int M;
+    // Stencil size
+    unsigned int n;
+    // Number of boundary nodes
+    unsigned int nb_bnd;
+    // N - nb_bnd
+    unsigned int NN;
+    // M - nb_bnd
+    unsigned int MM;
+
+    std::string dir_str; 
+
+    int solve_on_gpu; 
+
+    public: 
+    Poisson1D_PDE_DEV(Domain* grid, RBFFD* der, Communicator* comm, int use_gpu=0) 
+        // The 1 here indicates the solution will have one component
+        : ImplicitPDE(grid, der, comm, 1) , solve_on_gpu(use_gpu)
+    {   
+        N = grid_ref.getStencilsSize(); 
+        M = grid_ref.getNodeListSize(); 
+        n = grid_ref.getMaxStencilSize(); 
+        nb_bnd = grid_ref.getBoundaryIndicesSize();
+        NN = N - nb_bnd; 
+        MM = M - nb_bnd; 
+
+        LHS_host = new HOST_MAT_t(NN,MM,(NN)*n); 
+        RHS_host = new HOST_VEC_t(NN); 
+        U_exact_host = new HOST_VEC_t(M); 
+        U_approx_host = new HOST_VEC_t(N, 0); 
+
+        if (solve_on_gpu) {
+            LHS_dev = new DEV_MAT_t(NN,MM,(NN)*n); 
+            RHS_dev = new DEV_VEC_t(NN); 
+            U_exact_dev = new DEV_VEC_t(M); 
+            U_approx_dev = new DEV_VEC_t(N); 
+        }
+
+        char dir[FILENAME_MAX]; 
+        sprintf(dir, "output/%d_of_%d/", comm_ref.getRank()+1, comm_ref.getSize()); 
+
+        dir_str = string(dir); 
+        std::cout << "Making output dir: " << dir_str << std::endl;
+        boost::filesystem::create_directories(dir_str);
+    }
+
+    ~Poisson1D_PDE_DEV() {
+        delete(LHS_host); 
+        delete(RHS_host); 
+        delete(U_exact_host);
+        delete(U_approx_host);
+
+        if (solve_on_gpu) { 
+            delete(LHS_dev); 
+            delete(RHS_dev); 
+            delete(U_exact_dev);
+            delete(U_approx_dev);
+        }
+    }
+
+    // Catch the call to "solve()" and allow users to specify "solve_on_gpu=1"
+    // which requires memory allocation, copy to, solve, copy back operators
+    virtual void solve() {
+        if (solve_on_gpu) {
+            this->solve(*LHS_dev, *RHS_dev, *U_exact_dev, *U_approx_dev); 
+        } else {
+            this->solve(*LHS_host, *RHS_host, *U_exact_host, *U_approx_host); 
+        }
+    }
+
+
+
+    virtual void assemble() 
+    {
+
+        HOST_MAT_t& A = *LHS_host; 
+        HOST_VEC_t& F = *RHS_host;
+        HOST_VEC_t& U_exact = *U_exact_host;
+
+        std::cout << "Boundary nodes: " << nb_bnd << std::endl;
+
+        //------ RHS ----------
+
+        ManufacturedSolution UU; 
+
+        std::vector<NodeType>& nodes = grid_ref.getNodeList(); 
+
+        // We want U_exact to have the FULL solution. 
+        // F should only have the compressed problem. 
+        // NOTE: I am assuming the boundary nodes are first in the list. This
+        // works well for Dirichlet conditions. Perhaps we should elegantly
+        // handle them? 
+        for (unsigned int i = 0; i < nb_bnd; i++) {
+            NodeType& node = nodes[i]; 
+            double Xx = node.x(); 
+            double Yy = node.y(); 
+            double Zz = node.z(); 
+
+            U_exact[i] = UU.eval(Xx, Yy, Zz);// + 2*M_PI; 
+        }
+
+        for (unsigned int i = nb_bnd; i < N; i++) {
+            NodeType& node = nodes[i]; 
+            double Xx = node.x(); 
+            double Yy = node.y(); 
+            double Zz = node.z(); 
+
+            U_exact[i] = UU.eval(Xx, Yy, Zz); // + 2*M_PI; 
+            // Solving -lapl(u + const) = f = -lapl(u) + 0
+            // of course the lapl(const) is 0, so we will have a test to verify
+            // that our null space is closed. 
+            F[i-nb_bnd] = -UU.lapl(Xx, Yy, Zz); 
+        }
+
+        std::cout << "N = " << N << ", M = " << M << std::endl;
+        for (unsigned int i = N; i < M; i++) {
+            U_exact[i] = -1.; // Indicate that we need to receive data from other procs
+        }
+
+        // This should get values from neighboring processors into U_exact. 
+        this->sendrecvUpdates(U_exact,"U_exact"); 
+
+        //------ LHS ----------
+
+        // NOTE: assumes the boundary is sorted to the top of the node indices
+        for (unsigned int i = nb_bnd; i < N; i++) {
+            StencilType& sten = grid_ref.getStencil(i); 
+            double* lapl = der_ref.getStencilWeights(RBFFD::LAPL, i); 
+//            double lapl_fd[3] = {2, -1, -1}; 
+
+
+            for (unsigned int j = 0; j < n; j++) {
+                if (sten[j] < (int)nb_bnd) { 
+                    // Subtract the solution*weight from the element of the RHS. 
+                    F[i-nb_bnd] -= (U_exact[sten[j]] * ( -lapl[j] )); 
+                } else {
+                    // Offset by nb_bnd so we crop off anything related to the boundary
+                    A(i-nb_bnd,sten[j]-nb_bnd) = -lapl[j]; 
+                }
+            }
+        }    
+        
+        if (solve_on_gpu) 
+        { 
+            // Put all of the known system on the GPU. 
+            copy(A, *LHS_dev);
+            viennacl::copy(RHS_host->begin(),RHS_host->end(), RHS_dev->begin());
+            viennacl::copy(U_exact_host->begin(),U_exact_host->end(), U_exact_dev->begin());
+            viennacl::copy(U_approx_host->begin(),U_approx_host->end(), U_approx_dev->begin());
+        }
+    }
+
+    // Use GMRES to solve the linear system. 
+    template <class MAT_t, class VEC_t>
+        void solve(MAT_t& LHS, VEC_t& RHS, VEC_t& U_exact, VEC_t& U_approx_out)
+        {
+            // Solve on the CPU
+            int restart = 5; 
+            int krylov = 10;
+            double tol = 1e-8; 
+
+            // Tag has (tolerance, total iterations, number iterations between restarts)
+#if 1
+            viennacl::linalg::parallel_gmres_tag tag(comm_ref, grid_ref, tol, restart*krylov, krylov); 
+#else
+            viennacl::linalg::gmres_tag tag(tol, restart*krylov, krylov); 
+#endif 
+            viennacl::linalg::ilu0_precond< MAT_t > vcl_ilu0( LHS, viennacl::linalg::ilu0_tag() ); 
+#if 0
+            viennacl::io::write_matrix_market_file(vcl_ilu0.LU, dir_str + "ILU.mtx"); 
+            std::cout << "Wrote preconditioner to ILU.mtx\n";
+#endif        
+            std::cout << "GMRES Max Number of Iterations: " << tag.max_iterations() << std::endl;
+            std::cout << "GMRES Krylov Dim: " << tag.krylov_dim() << std::endl;
+            std::cout << "GMRES Max Number of Restarts (max_iter/krylov_dim - 1): " << tag.max_restarts() << std::endl;
+            std::cout << "GMRES Tolerance: " << tag.tolerance() << std::endl;
+
+#if 1
+            U_approx_out = viennacl::linalg::solve(LHS, RHS, tag); 
+#else 
+            U_approx_out = viennacl::linalg::solve(LHS, RHS, tag, vcl_ilu0); 
+#endif 
+            
+            if (tag.iters() < tag.max_iterations()) { 
+                std::cout << "\n[+++] Solver converged "; //<< tag.error() << " relative tolerance";       
+                std::cout << " after " << tag.iters() << " iterations" << std::endl << std::endl;
+            }
+            else
+            {
+                std::cout << "\n[XXX] Solver reached iteration limit " << tag.iters() << " before converging\n\n";
+            //    std::cout << " to " << tag.tolerance() << " relative tolerance " << std::endl << std::endl;
+            }
+
+
+            std::cout << "GMRES Iterations: " << tag.iters() << std::endl;
+            std::cout << "GMRES Iteration Limit: " << tag.max_iterations() << std::endl;
+            std::cout << "GMRES Residual Norm: " << tag.error() << std::endl;
+
+#if 0
+            std::cout << "GMRES Relative Tol: " << monitor.relative_tolerance() << std::endl;
+            std::cout << "GMRES Absolute Tol: " << monitor.absolute_tolerance() << std::endl;
+            std::cout << "GMRES Target Residual (Abs + Rel*norm(F)): " << monitor.tolerance() << std::endl;
+#endif 
+            checkNorms(U_approx_out, U_exact);
+        }
+
+    void checkNorms(DEV_VEC_t& sol, DEV_VEC_t& exact) {
+        DEV_VEC_t diff(sol.size()); 
+
+        viennacl::vector_range<DEV_VEC_t> exact_view( exact, viennacl::range(exact.size() - sol.size(), exact.size())); 
+
+        viennacl::linalg::sub(sol, exact_view, diff); 
+
+        std::cout << "Rel l1   Norm: " << viennacl::linalg::norm_1(diff) / viennacl::linalg::norm_1(exact) << std::endl;  
+        std::cout << "Rel l2   Norm: " << viennacl::linalg::norm_2(diff) / viennacl::linalg::norm_2(exact) << std::endl;  
+        std::cout << "Rel linf Norm: " << viennacl::linalg::norm_inf(diff) / viennacl::linalg::norm_inf(exact) << std::endl;  
+    }
+
+
+    void checkNorms(HOST_VEC_t& sol, HOST_VEC_t& exact) {
+        HOST_VEC_t diff = sol; 
+
+        boost::numeric::ublas::vector_range<HOST_VEC_t> exact_view( exact, boost::numeric::ublas::range(exact.size() - sol.size(), exact.size())); 
+
+        diff -= exact_view; 
+
+        std::cout << "Rel l1   Norm: " << viennacl::linalg::norm_1(diff) / viennacl::linalg::norm_1(exact) << std::endl;  
+        std::cout << "Rel l2   Norm: " << viennacl::linalg::norm_2(diff) / viennacl::linalg::norm_2(exact) << std::endl;  
+        std::cout << "Rel linf Norm: " << viennacl::linalg::norm_inf(diff) / viennacl::linalg::norm_inf(exact) << std::endl;  
+    }
+
+    void write_System ( )
+    {
+        write_to_file(*RHS_host, dir_str + "F.mtx"); 
+        write_to_file(*U_exact_host, dir_str + "U_exact.mtx"); 
+        viennacl::io::write_matrix_market_file(*LHS_host,dir_str + "LHS.mtx"); 
+    }
+
+    void write_Solution()
+    {
+        unsigned int nb_bnd = grid_ref.getBoundaryIndicesSize();
+
+        // IF we want to write details we need to copy back to host. 
+        HOST_VEC_t U_approx(M, 0); 
+        if (solve_on_gpu) { 
+            copy(U_approx_dev->begin(), U_approx_dev->end(), U_approx.begin()+nb_bnd);
+            write_to_file(U_approx, dir_str + "U_gpu.mtx"); 
+        } else { 
+            write_to_file(*U_approx_host, dir_str + "U_gpu.mtx"); 
+        }
+    }
+
+    virtual std::string className() {
+        return "Poisson1D_PDE_DEV"; 
+    }
+};
+
+#endif 

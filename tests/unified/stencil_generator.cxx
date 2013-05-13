12dc90008f2041fc0dc952f4137130466a68f288
Evan Bollig <bollig@gmail.com>
2013-05-13 18:22:17 -0500

    Works for older version of boost?

--- tests/unified/stencil_generator.cxx
+++ tests/unified/stencil_generator.cxx
@@ -162,7 +162,11 @@ int main(int argc, char** argv) {
 		// Assemble a DIRECTED graph that is the spadjacency_list our stencils
 //		typedef adjacency_list <boost::vecS, boost::setS, boost::bidirectionalS> Graph;
 
+#if 0
 		typedef boost::undirected_graph<> Graph;
+#else
+        typedef boost::adjacency_list < boost::vecS, boost::vecS, boost::undirectedS> Graph;
+#endif
 		Graph g;
 		Graph::vertex_descriptor vds[grid_size]; 
 

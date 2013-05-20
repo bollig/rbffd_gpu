33e8b41bf87b68cc0daa61a672628575513f3c42
Evan BOllig <bollig@gmail.com>
2013-05-20 00:19:41 -0500

    Fully loaded...looks like i missed track changes on previous editions

---
+++ tests/unified/evaluate_derivatives.cxx
@@ -0,0 +1,369 @@
+/*
+ * 
+ * Load metis domain
+ * Load all weights
+ * Load simple function (option to select 1's, sin(x), input file). 
+ * Apply weights to function using SpMV
+ * 
+ */
+
+#include <stdlib.h>
+#include <sstream>
+#include <map>
+#include <iostream> 
+
+#include "grids/grid_reader.h"
+#include "grids/domain.h"
+#include "grids/metis_domain.h"
+#include "rbffd/rbffd.h"
+#include "rbffd/derivative_tests.h"
+
+#include <boost/program_options.hpp>
+
+#include "timer_eb.h"
+
+#include <mpi.h> 
+
+using namespace std;
+using namespace EB;
+using namespace boost; 
+
+namespace po = boost::program_options;
+
+//----------------------------------------------------------------------
+//NOTE: EVERYTHING BELOW IN MAIN WAS COPIED FROM heat_regulargrid_2d/main.cpp
+//----------------------------------------------------------------------
+
+int main(int argc, char** argv) {
+
+	TimerList tm;
+
+	tm["total"] = new Timer("[Main] Total runtime for this proc");
+	tm["grid"] = new Timer("[Main] Grid generation");
+	tm["gridReader"] = new Timer("[Main] Grid Reader Load File From Disk");
+	tm["loadGrid"] = new Timer("[Main] Load Grid (and Stencils) from Disk");
+	tm["writeGrid"] = new Timer("[Main] Write Grid (and Stencils) to Disk");
+	tm["stencils"] = new Timer("[Main] Stencil generation");
+	tm["writeStencils"] = new Timer("[Main] Write Stencils to Disk");
+	tm["settings"] = new Timer("[Main] Load settings");
+	tm["derSetup"] = new Timer("[Main] Setup RBFFD class");
+	tm["weights"] = new Timer("[Main] Compute Weights");
+	tm["loadWeights"] = new Timer("[Main] Read weights from file");
+	tm["tests"] = new Timer("[Main] Derivative tests");
+
+	tm["total"]->start();
+
+	//-----------------
+	tm["settings"]->start();
+
+	// Declare the supported options.
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "produce help message")
+		("debug,d", "enable verbose debug messages")
+		("grid_filename,g", po::value<string>(), "Grid filename (flat file, tab delimited columns). Required.")
+		("grid_num_cols,c", po::value<int>(), "Number of columns to expect in the grid file (X,Y,Z first). Note: columns in grid file, not problem dimensions")
+		("grid_size,N", po::value<int>(), "Number of nodes to expect in the grid file") 
+		("grid_dim,D", po::value<int>(), "Grid dimensions. Note: dimensions may be fewer than columns in grid file.") 
+		("stencil_size,n", po::value<int>(), "Number of nodes per stencil (assume all stencils are the same size)")
+		("partition_filename,p", po::value<string>(), "METIS Output Partition Filename (*.part.<P-processors>)")
+		("use_hyperviscosity", po::value<int>(), "Enable the computation of Hyperviscosity weights")
+		("hv_k", po::value<int>(), "Power of hyperviscosity")
+		("hv_gamma", po::value<double>(), "Scaling parameter on hyperviscosity")
+		("eps_c1", po::value<double>(), "Choose Epsilon as function of eps_c1 and eps_c2")
+		("eps_c2", po::value<double>(), "Choose Epsilon as function of eps_c1 and eps_c2")
+		("weight_method", po::value<int>(), "Set the method used to compute weights: 0 -> Direct Inversion of Ax=B; 1 -> ContourSVD") 
+		("ascii_weights,a", "Write weights in ASCII Matrix Market format (Default: off)") 
+		("weights,w", po::value<unsigned int>(), "Select the weights to compute. Argument should be an unsigned integer similar to the chmod comand. For example, the combination of weights X, Y, Z are 0x1 | 0x2 | 0x4 -> 0x7 == 7. Current choices are: X=0x1, Y=0x2, Z=0x4, LAPL=0x8, R=0x10, HV=0x20, LAMBDA=0x40, THETA=0x80, LSFC=0x100, XSFC=0x200, YSFC=0x400, ZSFC=0x800, ALT_XSFC=0x1000, ALT_YSFC=0x2000, ALT_ZSFC=0x4000, INTERP=0x8000")
+		;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);    
+
+	if (vm.count("help")) {
+		cout << desc << "\n";
+		return 1;
+	}
+
+	int debug = 0;
+	if (vm.count("debug")) {
+		debug = 1; 
+	}
+
+	int ascii_weights = 0;
+	if (vm.count("ascii_weights")) {
+		ascii_weights = 1;
+	}
+
+	string grid_filename; 
+	if (vm.count("grid_filename")) {
+		grid_filename = vm["grid_filename"].as<string>(); 
+		cout << "Loading grid: " << grid_filename<< ".\n";
+	} else {
+		cout << "ERROR: grid_filename not specified\n";
+		exit(-1); 
+	}
+
+	string partition_filename; 
+	bool part_file_loaded = false;
+	if (vm.count("partition_filename")) {
+		partition_filename = vm["partition_filename"].as<string>(); 
+		cout << "Loading partition file: " << partition_filename << ".\n";
+		part_file_loaded = true; 
+	} else {
+		cout << "WARNING: partition_filename not specified, defaulting to all stencils for each processor\n";
+	}
+
+	int grid_dim = 3;
+	if (vm.count("grid_dim")) {
+		grid_dim = vm["grid_dim"].as<int>(); 
+	}
+
+	int grid_num_cols; 
+	if (vm.count("grid_num_cols")) {
+		grid_num_cols = vm["grid_num_cols"].as<int>(); 
+		cout << "Number of expected columns: " << grid_num_cols << ".\n";
+	} else {
+		cout << "grid_num_cols was not set. Defaulting to grid_dim = " << grid_dim << ".\n";
+		grid_num_cols = grid_dim;
+	}
+
+	int grid_size; 
+	if (vm.count("grid_size")) {
+		grid_size = vm["grid_size"].as<int>(); 
+		cout << "Number of expected nodes: " << grid_size << ".\n";
+	} else {
+		cout << "ERROR: grid_size was not set.\n";
+		exit(-2); 
+	}
+	
+	// Select all derivative types (just in case)
+	// Equivalent to: RBFFD::X | RBFFD::Y | RBFFD::Z | RBFFD::LAPL | [...] | RBFFD::INTERP
+	unsigned int weight_choices = (0x1 << RBFFD::NUM_DERIVATIVE_TYPES) - 1;
+	if (vm.count("weights")) {
+		weight_choices = vm["weights"].as<unsigned int>(); 
+		cout << "Weight choices overridden to compute: " << weight_choices << ".\n";
+	} else {
+		cout << "Computing all weights: " << weight_choices << ", " << RBFFD::NUM_DERIVATIVE_TYPES <<  std::endl;
+	}
+
+	int stencil_size; 
+	if (vm.count("stencil_size")) {
+		stencil_size = vm["stencil_size"].as<int>(); 
+		cout << "Number of nodes per stencil: " << stencil_size << ".\n";
+	} else {
+		cout << "ERROR: stencil_size was not set.\n";
+		exit(-3); 
+	}
+
+	int use_hyperviscosity = 0; 
+	int hv_k = -1;
+	double hv_gamma = 0;
+	if (vm.count("use_hyperviscosity")) {
+		use_hyperviscosity = vm["use_hyperviscosity"].as<int>(); 
+		cout << "Use Hyperviscosity: " << use_hyperviscosity<< ".\n";
+		if (vm.count("hv_k")) {
+			hv_k = vm["hv_k"].as<int>(); 
+			cout << "HV_K : " << use_hyperviscosity<< ".\n";
+		} else { 
+			cout << "ERROR: hv_k required for use_hyperviscosity\n";
+			exit(-3); 
+		}	
+		if (vm.count("hv_gamma")) {
+			hv_gamma = vm["hv_gamma"].as<double>(); 
+			cout << "hv_gamma: " << hv_gamma << ".\n";
+		} else { 
+			cout << "ERROR: hv_gamma required for use_hyperviscosity\n";
+			exit(-3); 
+		}
+	}
+
+
+
+	double eps_c1 = 1.;
+	double eps_c2 = 0.;
+	bool eps_c1_c2 = false; 
+	if (vm.count("eps_c1")) {
+		eps_c1 = vm["eps_c1"].as<double>(); 
+		cout << "Epsilon c1: " << eps_c1 << ".\n";
+		eps_c1_c2 = true;	
+	} else {
+		cout << "ERROR: eps_c1 was not set.\n";
+		exit(-3); 
+	}
+	if (vm.count("eps_c2")) { 
+		if (eps_c1_c2) {
+			eps_c2 = vm["eps_c2"].as<double>(); 
+			cout << "Epsilon c2: " << eps_c2 << ".\n";
+		} else {
+			cout << "ERROR: eps_c2 requires eps_c1\n"; 
+			exit(-3); 
+		}
+	}
+
+	int weight_method = 0; 
+	if (vm.count("weight_method")) {
+		weight_method = vm["weight_method"].as<int>(); 
+		cout << "Weight Calculation Method: " << weight_method << ".\n";
+	}
+
+
+#if 1
+	MPI::Init(argc, argv);
+	int mpi_rank = MPI::COMM_WORLD.Get_rank();
+	int mpi_size = MPI::COMM_WORLD.Get_size();
+#else 
+	int mpi_rank = 0; 
+	int mpi_size = 1;
+#endif 	
+
+	Grid* grid;
+
+	// Master process loads grid in case we we need collectives like MPI_Reduce
+	if (!mpi_rank) { 
+		tm["gridReader"]->start();
+		grid = new GridReader(grid_filename, grid_num_cols, grid_size);
+		grid->setMaxStencilSize(stencil_size);
+		tm["gridReader"]->stop();
+
+		tm["loadGrid"]->start();
+		Grid::GridLoadErrType err = grid->loadFromFile(grid_filename);
+		tm["loadGrid"]->stop();
+		if ((err == Grid::NO_GRID_FILES) || (err == Grid::NO_STENCIL_FILES)) {
+			std::cout << "ERROR: Master process unable to read grid. Exiting..." << std::endl;
+			exit(-1);
+		}
+	}
+
+	// Less memory efficient but will get the job done: 
+	// Every proc can: 
+	// 	read whole grid, 
+	// 	read whole stencils, 
+	// 	compute weights for subset of grid
+	// 	write subset of weights to weights_*_...part<rank>_of_<size>
+	// NOTE: no need to determine sets Q,O,R,B, etc. here. 
+
+
+	// Similar to GridReader. Although it should not read in the stencils unless they end in a rank #. 
+
+	Domain* subdomain; 
+	subdomain = new METISDomain(mpi_rank, mpi_size, grid_dim, stencil_size); 
+	Grid::GridLoadErrType d_load_err = subdomain->loadFromFile(); 
+	if (d_load_err) { 
+		std::cout << "ERROR: process " << mpi_rank << " could not load domain\n";
+		exit(-1);
+	}
+
+	std::cout << "DECOMPOSED\n";
+#if 0
+	if (debug) {
+		subdomain->printVerboseDependencyGraph();
+		subdomain->printNodeList("All Centers Needed by This Process");
+
+		printf("CHECKING STENCILS: ");
+		for (int irbf = 0; irbf < (int)subdomain->getStencilsSize(); irbf++) {
+			//  printf("Stencil[%d] = ", irbf);
+			StencilType& s = subdomain->getStencil(irbf);
+			if (irbf == s[0]) {
+				//	printf("PASS\n");
+				//    subdomain->printStencil(s, "S");
+			} else {
+				printf("FAIL on stencil %d\n", irbf);
+
+				tm["total"]->stop();
+				tm.printAll();
+				tm.writeAllToFile();
+
+				exit(EXIT_FAILURE);
+			}
+		}
+		printf("OK\n");
+	}
+#endif 
+
+	tm["derSetup"]->start();
+	RBFFD* der = new RBFFD(weight_choices, subdomain, grid_dim, mpi_rank);
+
+	der->setUseHyperviscosity(use_hyperviscosity);
+	// If both are zero assume we havent set anything
+	if (eps_c1 || eps_c2) {
+		der->setEpsilonByParameters(eps_c1, eps_c2);
+	} else {
+		der->setEpsilonByStencilSize();
+	}
+	if (hv_k != -1) {
+		der->setHVScalars(hv_k, hv_gamma);
+	}
+	der->setWeightType((RBFFD::WeightType)weight_method);
+	tm["derSetup"]->stop();
+
+	printf("Attempting to load Stencil Weights\n");
+
+	// Try loading all the weight files
+	tm["loadWeights"]->start();
+	der->overrideFileDetail(true);
+	der->setAsciiWeights(ascii_weights);
+	int load_err = der->loadAllWeightsFromFile();
+	tm["loadWeights"]->stop();
+
+#if 0
+	//if (settings->GetSettingAs<int>("RUN_DERIVATIVE_TESTS", ProjectSettings::optional, "1")) {
+		bool weightsPreComputed = false; // X,Y,Z and LAPL are required for tests and were not selected
+		bool exitIfTestFailed = 1; //settings->GetSettingAs<int>("BREAK_ON_DERIVATIVE_TESTS", ProjectSettings::optional, "1");
+		bool exitIfEigTestFailed = 1; //settings->GetSettingAs<int>("BREAK_ON_EIG_TESTS", ProjectSettings::optional, "1");
+		bool compute_eigenvalues = false; 
+		tm["tests"]->start(); 
+		// The test class only computes weights if they havent been done already
+		DerivativeTests* der_test = new DerivativeTests(grid_dim, der, subdomain, weightsPreComputed);
+		#if 0
+		if (use_gpu) {
+			// Applies weights on both GPU and CPU and compares results for the first 10 stencils
+			der_test->compareGPUandCPUDerivs(10);
+		}
+		#endif
+		// Test approximations to derivatives of functions f(x,y,z) = 0, x, y, xy, etc. etc.
+		der_test->testAllFunctions(exitIfTestFailed);
+		// For now we can only test eigenvalues on an MPI size of 1 (we could distribute with Par-Eiegen solver)
+		if (mpi_size == 1) {
+			if (compute_eigenvalues) 
+			{
+				// FIXME: why does this happen? Perhaps because X Y and Z are unidirectional? 
+				// Test X and 4 eigenvalues are > 0
+				// Test Y and 30 are > 0
+				// Test Z and 36 are > 0
+				// NOTE: the 0 here implies we compute the eigenvalues but do not run the iterations of the random perturbation test
+				//                der_test->testEigen(RBFFD::LAPL, exitIfEigTestFailed, 0);                
+				// NOTE: testHyperviscosity boolean allow us to write the
+				// effect of hyperviscosity on the eigenvalues
+				der_test->testEigen(RBFFD::LAMBDA, exitIfEigTestFailed, 0);
+			}
+		}
+		tm["tests"]->stop();
+	//}
+
+
+		MPI_Finalize();
+		exit(0);
+#endif 
+#if 1
+	if (!mpi_rank) { 
+		delete(grid);
+		std::cout << "Deleted grid\n";
+	}
+	delete(subdomain); 
+	std::cout << "Deleted subdomain\n";
+#endif 
+
+	tm["total"]->stop();
+	tm.printAll();
+
+
+	std::cout << "----------------  END OF MAIN ------------------\n";
+	tm.writeAllToFile("time_log.stencils");
+	tm.clear();
+	MPI::Finalize();
+
+	return 0;
+}
+//----------------------------------------------------------------------
--- tests/unified/run.sh
+++ tests/unified/run.sh
@@ -20,7 +20,7 @@
 # -w : neighbor query method (0:LSH, 1:KDTree, 2:BruteForce)
 # -l : LSH grid size (<val>^3 overlaid grid)
 
-export NPROC=16
+export NPROC=4
 
 # Read grid, generate stencils (Note: -c 4 is required because MD nodes have 4 cols)
 ./sten_gen.x -g ~/sphere_grids/md063.04096 -c 4 -N 4096 -n 17 -w 0 -l 100
@@ -29,8 +29,13 @@ export NPROC=16
 gpmetis metis_stencils.graph ${NPROC}
 
 # Compute weights for each partition (WARNING: may produce large output files)
-echo "STARTING WEIGHTS"
-mpirun -np ${NPROC} ./compute_weights.x -g input_grid.ascii -a -N 4096 -n 17 --eps_c1 0.035 --eps_c2 0.1 -p metis_stencils.graph.part.${NPROC}
+mpirun -np ${NPROC} ./compute_weights.x -w 15 -g input_grid.ascii -a -N 4096 -n 17 --eps_c1 0.035 --eps_c2 0.1 -p metis_stencils.graph.part.${NPROC}
+
+mpirun -output-filename evallog -np ${NPROC} ./evaluate_derivatives.x -g input_grid.ascii -a -N 4096 -n 17 --eps_c1=0.035 --eps_c2=0.1 -w 15
+
+
+
+
 
 #mpirun -output-filename runlog -np 2 ./compute_weights.x -g input_grid.ascii -c 4 -N 4096 -n 17 --eps_c1 0.035 --eps_c2 0.1 -p lsh_100_stencils.graph.part.2
 

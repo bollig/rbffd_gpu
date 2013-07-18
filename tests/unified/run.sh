0a6ac9cbd1c0a02d5b12ccd70b0c5de53b48d46f
Evan Bollig <bollig@gmail.com>
2013-07-18 18:09:05 -0500

    copied out of build

--- tests/unified/run.sh
+++ tests/unified/run.sh
@@ -10,8 +10,8 @@
 # DONE:	read stencils
 # DONE:	compute weights
 # DONE:	write weights
-# TODO:	assemble 
-# TODO:	solve
+# TODO:	assemble GPU
+# TODO:	solve GPU
 
 
 # -c : number of cols in the grid file (3:X,Y,Z; 4:X,Y,Z,ignore)
@@ -20,22 +20,58 @@
 # -w : neighbor query method (0:LSH, 1:KDTree, 2:BruteForce)
 # -l : LSH grid size (<val>^3 overlaid grid)
 
-export NPROC=4
 
-# Generate a regular grid of size 100^3 
-#./gen_regular_grid.x -x 100 -y 100 -z 100
+MY_MPI_EXE="mpirun -r ssh -l"
+date
 
+MD=165
+N=1000
+STEN_SIZE=101
 
-# Read grid, generate stencils (Note: -c 4 is required because MD nodes have 4 cols)
-./sten_gen.x -g ~/sphere_grids/md063.04096 -c 4 -N 4096 -n 17 -w 0 -l 100
+NPROC=4
+#NPROC=1
+#~/sphere_grids/md${MD}.${N}
 
-# Decompose domain for parallel run into NPROC partitions
-gpmetis metis_stencils.graph ${NPROC}
+METIS_FILE=metis_stencils.graph.part.${NPROC}
+JOB_RAN_FILE=job_ran
 
-# Compute weights for each partition (WARNING: may produce large output files)
-mpirun -np ${NPROC} ./compute_weights.x -w 15 -g input_grid.ascii -a -N 4096 -n 17 --eps_c1 0.035 --eps_c2 0.1 -p metis_stencils.graph.part.${NPROC}
+TEST_TYPE=reg
+NEW_WORKDIR=./${TEST_TYPE}_${N}_${STEN_SIZE}_${NPROC}proc
 
-mpirun -np ${NPROC} ./evaluate_derivatives.x -g input_grid.ascii -a -N 4096 -n 17 --eps_c1=0.035 --eps_c2=0.1 -w 15
+GRID_FILE=$HOME/GRIDS/regular/10_cubed/regulargrid_10x_10y_10z_final.ascii
+
+mkdir -p $NEW_WORKDIR
+cd $NEW_WORKDIR
+
+if [ ! -f $JOB_RAN_FILE ] 
+then
+
+	# Generate a regular grid of size 100^3 
+	#./gen_regular_grid.x -x 100 -y 100 -z 100
+
+	# Read grid, generate stencils (Note: -c 4 is required because MD nodes have 4 cols)
+	../sten_gen.x -g ${GRID_FILE} -c 3 -N ${N} -n ${STEN_SIZE} -w 0 -l 100
+
+	echo "sten_gen Exit status: $?"
+
+	# Decompose domain for parallel run into NPROC partitions
+	gpmetis metis_stencils.graph ${NPROC}
+
+	echo "gpmetis Exit status: $?"
+
+	#	${MY_MPI_EXE} -np ${NPROC} hostname
+	# echo "hostname Exit status: $?"
+
+	# add -l for verbose logging of output 
+	${MY_MPI_EXE} -np ${NPROC} ../compute_weights.x -w 15 -g input_grid.ascii -a -N ${N} -n ${STEN_SIZE} --eps_c1 0.035 --eps_c2 0.1 -p metis_stencils.graph.part.${NPROC}
+
+	echo "compute_weights Exit status: $?"
+	touch $JOB_RAN_FILE
+fi
+
+${MY_MPI_EXE} -np ${NPROC} ../evaluate_derivatives.x -g input_grid.ascii -a -N ${N}  -n ${STEN_SIZE} --eps_c1=0.035 --eps_c2=0.1 -w 15 
+
+echo "evaluate_derivatives Exit status: $?"
 
 
 #echo "demonstrating 2D regular grid: " 
@@ -47,3 +83,4 @@ mpirun -np ${NPROC} ./evaluate_derivatives.x -g input_grid.ascii -a -N 4096 -n 1
 
 #mpirun -output-filename runlog -np 2 ./compute_weights.x -g input_grid.ascii -c 4 -N 4096 -n 17 --eps_c1 0.035 --eps_c2 0.1 -p lsh_100_stencils.graph.part.2
 
+date

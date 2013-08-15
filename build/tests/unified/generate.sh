f7f1925adf10287fb49cfc51766d979f060b8380
Evan BOllig <bollig@gmail.com>
2013-08-14 22:14:01 -0500

    Tested generation up to 200 cubed. going to add the files to the grids repo. 200^3 took about an hour. "

--- build/tests/unified/generate.sh
+++ build/tests/unified/generate.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-STEN=16
+STEN=50
 
 N=10
 echo ${N}
 mkdir -p ${N}_cubed 
 cd ${N}_cubed
-../gen_reg_grid.x -x ${N} -y ${N} -z ${N} -s ${STEN} 
-cd ../
-#$(seq 20 20 200)
-for N in 10
+../gen_reg_grid.x -x ${N} -y ${N} -z ${N} -s ${STEN}  -l 100
+cd -
+
+for N in $(seq 20 20 200)
 do
 	echo ${N}
 	mkdir -p ${N}_cubed 
 	cd ${N}_cubed
-	../../gen_reg_grid.x -x ${N} -y ${N} -z ${N} -s ${STEN} -l 10
-	cd ../
+	../gen_reg_grid.x -x ${N} -y ${N} -z ${N} -s ${STEN} -l 100
+	cd -
 done
